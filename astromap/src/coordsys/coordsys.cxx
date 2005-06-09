4a83b14aa3e12aa47403dcc4c981d8bdb0e31509
bnikolic <>
2005-06-09 12:44:21 +0000

    *** empty log message ***

---
+++ astromap/src/coordsys/coordsys.cxx
@@ -0,0 +1,13 @@
+/*
+  Bojan Nikolic
+  $Id: coordsys.cxx,v 1.1 2005/06/09 13:44:22 bnikolic Exp $
+
+*/
+
+#include "coordsys.hxx"
+
+namespace AstroMap {
+
+
+
+}
---
+++ astromap/src/coordsys/coordsys.hxx
@@ -0,0 +1,21 @@
+/*
+  Bojan Nikolic
+  $Id: coordsys.hxx,v 1.1 2005/06/09 13:44:22 bnikolic Exp $
+
+  
+*/
+#ifndef _ASTROMAP_COORDSYS_HXX__
+#define _ASTROMAP_COORDSYS_HXX__
+
+namespace AstroMap {
+
+  
+  /*!
+   *
+   */
+  
+
+
+}
+
+#endif
