82d36795ba48705c492971d3c71c1215c0e0f7cd
bnikolic <>
2005-06-21 13:27:42 +0000

    *** empty log message ***

--- oof/src/telgeo/cassegrain.cxx
+++ oof/src/telgeo/cassegrain.cxx
@@ -1,8 +1,43 @@
 /*!
   Bojan Nikolic
-  $Id: cassegrain.cxx,v 1.1 2005/05/24 20:34:16 bnikolic Exp $
+  $Id: cassegrain.cxx,v 1.2 2005/06/21 14:27:43 bnikolic Exp $
 
 */
 
 
 #include "cassegrain.hxx"
+
+#include <cmath>
+
+#include <bndebug.hxx>
+
+
+
+namespace OOF {
+
+  double SecDeltaPath (double x, double y,
+		       double dX, double dY , double dZ,
+		       const CassegrainGeo & geo)
+  {
+    
+    /*! Think about  dX, dY a little later.... */
+    ENFORCE( dX==0 && dY == 0 );
+
+    double delta=0;
+
+    const double r= sqrt (x*x + y*y);
+
+    /* Change of path due to defocus (dZ) */
+    const double a = r / 2.0 / geo.PrimF;
+    const double b = r / 2.0 / geo.CasF ;
+
+    delta += dZ * ( (1 - a*a)/(1 + a*a) + (1 - b*b)/(1 + b*b) );
+
+    return delta;
+
+  }
+
+  
+
+
+}
--- oof/src/telgeo/cassegrain.hxx
+++ oof/src/telgeo/cassegrain.hxx
@@ -1,21 +1,39 @@
 /*!
   Bojan Nikolic
-  $Id: cassegrain.hxx,v 1.1 2005/05/24 20:34:16 bnikolic Exp $
+  $Id: cassegrain.hxx,v 1.2 2005/06/21 14:27:43 bnikolic Exp $
 
 */
 #ifndef _OOF_TELGEO_CASSEGRAIN_HXX__
 #define _OOF_TELGEO_CASSEGRAIN_HXX__
+
 namespace OOF {
 
-  
+  /*! Describes the geometry when using a cassegrain focus */
   struct CassegrainGeo {
     
+    /*! Radius of the primary reflector */
     double PrimRadius;
+    
+    /*! Focal length of the primary */
     double PrimF;
     
+    /*! Radius of the secondary */
     double SecRadius;
+
+    /*! Effective focal length at cassegrain focus*/
     double CasF;
   };
+
+  /*! Calculate path change due to movement of the secondary mirror
+   *  from optimal position.
+   *
+   *  x,y is the positions in the aperture plane to calculate the path
+   *  change.  
+   *  dX, dY , dZ are the movements of the secondary mirror. 
+   */
+  double SecDeltaPath (double x, double y,
+		       double dX, double dY , double dZ,
+		       const CassegrainGeo & geo);
 
   
 }
