6fcfa8d10891f2f2148e606e548747334ebdffc5
bnikolic <>
2005-07-02 19:43:51 +0000

    added dependance on bnfits

--- astromap/src/dataser/dsfileops.cxx
+++ astromap/src/dataser/dsfileops.cxx
@@ -1,15 +1,29 @@
 /*!
   Bojan Nikolic
-  $Id: dsfileops.cxx,v 1.1 2005/07/02 20:38:09 bnikolic Exp $
+  $Id: dsfileops.cxx,v 1.2 2005/07/02 20:43:51 bnikolic Exp $
 
 */
 
 #include "dsfileops.hxx"
 
+#include <memory>
+#include <bndebug.hxx>
+
+#include "dataseries.hxx"
+
+
 namespace AstroMap {
 
 
-  
+  DataSeries * LoadFITSDS( const char * fname , unsigned extno)
+  {
+
+    
+    std::auto_ptr<DataSeries> res (ENFORCE(new DataSeries(0 )));
+
+    return res.release();
+
+  }
 
 
 
