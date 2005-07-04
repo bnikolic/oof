1894f7bb8b8b707faa551a3900aa2fd60fe6fedf
bnikolic <>
2005-07-04 11:11:14 +0000

    make a start on the dstomap module

---
+++ astromap/src/dataser/dstomap.cxx
@@ -0,0 +1,14 @@
+/*
+  Bojan Nikolic
+  $Id: dstomap.cxx,v 1.1 2005/07/04 12:11:14 bnikolic Exp $
+*/
+
+#include "dstomap.hxx"
+
+namespace AstroMap {
+
+
+
+}
+
+
---
+++ astromap/src/dataser/dstomap.hxx
@@ -0,0 +1,17 @@
+/*
+  Bojan Nikolic
+  $Id: dstomap.hxx,v 1.1 2005/07/04 12:11:14 bnikolic Exp $
+
+  Drizzle a dataseries into a map
+*/
+
+#ifndef _ASTROMAP_DSTOMAP_HXX__
+#define _ASTROMAP_DSTOMAP_HXX__
+
+namespace AstroMap {
+
+
+
+}
+
+#endif
