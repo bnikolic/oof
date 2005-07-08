1d087e25d0115d8c0b149f6de95a796721fae008
bnikolic <>
2005-07-08 20:50:37 +0000

    *** empty log message ***

---
+++ astromap/src/coordsys/csops.cxx
@@ -0,0 +1,16 @@
+/*
+  Bojan Nikolic
+  $Id: csops.cxx,v 1.1 2005/07/08 21:50:38 bnikolic Exp $
+
+  Coordinate system operations
+*/
+
+#include "csops.hxx"
+
+namespace AstroMap {
+
+
+
+}
+
+
---
+++ astromap/src/coordsys/csops.hxx
@@ -0,0 +1,15 @@
+/*
+  Bojan Nikolic
+  $Id: csops.hxx,v 1.1 2005/07/08 21:50:38 bnikolic Exp $
+
+  Coordinate system operations
+*/
+#ifndef __ASTROMAP_CSOPS_HXX__
+#define __ASTROMAP_CSOPS_HXX__
+
+namespace AstroMap {
+
+
+}
+
+#endif
