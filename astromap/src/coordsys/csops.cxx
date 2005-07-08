6c208a16fd52ddfe78bfc7a0ce743eeaf3fd8ca1
bnikolic <>
2005-07-08 21:04:57 +0000

    MkApCS works

--- astromap/src/coordsys/csops.cxx
+++ astromap/src/coordsys/csops.cxx
@@ -1,15 +1,39 @@
 /*
   Bojan Nikolic
-  $Id: csops.cxx,v 1.1 2005/07/08 21:50:38 bnikolic Exp $
+  $Id: csops.cxx,v 1.2 2005/07/08 22:04:57 bnikolic Exp $
 
   Coordinate system operations
 */
 
 #include "csops.hxx"
 
+#include <bndebug.hxx>
+
+#include "../astromap.hxx"
+#include "coordsys.hxx"
+#include "lincordsys.hxx"
+
+
 namespace AstroMap {
 
+  void MkApCS(Map &m , double halflen)
+  {
 
+    // For the time being assumed we have a linear coordinate system;
+    LinCS * lcs = dynamic_cast<LinCS *>( m.cs );
+    // This will cause an abort if dynamic cast failed
+    ENFORCE(lcs);
+
+    lcs->TM[0] = halflen * 2 / m.nx;
+    lcs->TM[1] = 0;
+    lcs->TM[2] = -1.0 * halflen  ;
+    
+    lcs->TM[3] = 0;
+    lcs->TM[4] = halflen * 2 / m.ny;
+    lcs->TM[5] = -1.0 *  halflen  ;
+
+
+  }
 
 }
 
--- astromap/src/coordsys/csops.hxx
+++ astromap/src/coordsys/csops.hxx
@@ -1,6 +1,6 @@
 /*
   Bojan Nikolic
-  $Id: csops.hxx,v 1.1 2005/07/08 21:50:38 bnikolic Exp $
+  $Id: csops.hxx,v 1.2 2005/07/08 22:04:57 bnikolic Exp $
 
   Coordinate system operations
 */
@@ -8,6 +8,13 @@
 #define __ASTROMAP_CSOPS_HXX__
 
 namespace AstroMap {
+
+  class Map;
+
+  /*! Assign a coordinate system centred at zero, square in total
+    dimension and with the specified halflength
+  */
+  void MkApCS(Map &m, double halflen);
 
 
 }
