42cb9531403c4b9135e3a2323ea755fce55a4a72
bnikolic <>
2005-07-15 19:49:32 +0000

    begin added one-d root finding

---
+++ bnlib/src/onedroot.cxx
@@ -0,0 +1,17 @@
+/*
+  Bojan Nikolic
+  $Id: onedroot.cxx,v 1.1 2005/07/15 20:49:32 bnikolic Exp $
+
+*/
+
+#include "onedroot.hxx"
+
+#include "unaryfn.hxx"
+
+namespace BNLib {
+
+
+
+}
+
+
---
+++ bnlib/src/onedroot.hxx
@@ -0,0 +1,17 @@
+/*
+  Bojan Nikolic
+  $Id: onedroot.hxx,v 1.1 2005/07/15 20:49:32 bnikolic Exp $
+
+  One dimensional root finding
+*/
+#ifndef __BNLIB_ONEDROOT_HXX__
+#define __BNLIB_ONEDROOT_HXX__
+
+namespace BNLib {
+
+  class UnaryDD;
+
+
+}
+
+#endif
