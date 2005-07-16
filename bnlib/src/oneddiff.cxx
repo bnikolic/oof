0f70f5b4d872f15e6c1343d4acb511b9ab5dce22
bnikolic <>
2005-07-16 14:50:17 +0000

    first try at the differentitation class

--- bnlib/src/oneddiff.cxx
+++ bnlib/src/oneddiff.cxx
@@ -1,12 +1,28 @@
 /*
   Bojan Nikolic
-  $Id: oneddiff.cxx,v 1.1 2005/07/16 15:34:41 bnikolic Exp $
+  $Id: oneddiff.cxx,v 1.2 2005/07/16 15:50:17 bnikolic Exp $
   
 */
 
 #include "oneddiff.hxx"
 
 namespace BNLib {
+
+  LinDiff::LinDiff ( UnaryDD &fn, double eps ):
+    fn(fn),
+    eps(eps)
+  {
+  }
+
+  double LinDiff::operator() (double x)  
+  {
+    // Use a symetric difference here 
+    double y1 = fn(x+eps);
+    double y2 = fn(x-eps);
+
+    return (y1-y2) / ( 2* eps );
+
+  }
   
 
 
--- bnlib/src/oneddiff.hxx
+++ bnlib/src/oneddiff.hxx
@@ -1,6 +1,6 @@
 /*
   Bojan Nikolic
-  $Id: oneddiff.hxx,v 1.1 2005/07/16 15:34:41 bnikolic Exp $
+  $Id: oneddiff.hxx,v 1.2 2005/07/16 15:50:17 bnikolic Exp $
   
   One dimensional numeric differentiation
 */
@@ -8,9 +8,29 @@
 #ifndef _BNLIB_ONEDDIFF_HXX__
 #define _BNLIB_ONEDDIFF_HXX__
 
+#include "unaryfn.hxx"
+
 namespace BNLib {
 
-  
+  /*! Returns the first differential of the supplied function */
+  class LinDiff : public UnaryDD {
+    
+    /*! The function to be differentiated */
+    UnaryDD &fn;
+
+    /*! Forward difference amount to be used */
+    double eps;
+    
+  public:
+    
+    LinDiff ( UnaryDD &fn, double eps );
+
+    /* ---- Inherited functions ------*/
+
+    /*! Returs the differential at x */
+    virtual double operator() (double x)  ;
+
+  };
   
 
 
