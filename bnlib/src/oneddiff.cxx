8b074531a76e268ca3eb41f32899194386b42d73
bnikolic <>
2005-07-16 14:34:41 +0000

    *** empty log message ***

---
+++ bnlib/src/oneddiff.cxx
@@ -0,0 +1,16 @@
+/*
+  Bojan Nikolic
+  $Id: oneddiff.cxx,v 1.1 2005/07/16 15:34:41 bnikolic Exp $
+  
+*/
+
+#include "oneddiff.hxx"
+
+namespace BNLib {
+  
+
+
+
+}
+
+
---
+++ bnlib/src/oneddiff.hxx
@@ -0,0 +1,20 @@
+/*
+  Bojan Nikolic
+  $Id: oneddiff.hxx,v 1.1 2005/07/16 15:34:41 bnikolic Exp $
+  
+  One dimensional numeric differentiation
+*/
+
+#ifndef _BNLIB_ONEDDIFF_HXX__
+#define _BNLIB_ONEDDIFF_HXX__
+
+namespace BNLib {
+
+  
+  
+
+
+
+}
+
+#endif
