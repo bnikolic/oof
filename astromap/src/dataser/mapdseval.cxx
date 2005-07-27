d68382712d32eca5cf1551ea4dc2e40c1aefd8b7
bnikolic <>
2005-07-27 18:08:42 +0000

    added files that will contain code to interpolate maps to positions in a data series

---
+++ astromap/src/dataser/mapdseval.cxx
@@ -0,0 +1,15 @@
+/*
+  Bojan Nikolic
+  $Id: mapdseval.cxx,v 1.1 2005/07/27 19:08:43 bnikolic Exp $
+
+*/
+
+#include "mapdseval.hxx"
+
+
+namespace AstroMap {
+
+
+}
+
+
---
+++ astromap/src/dataser/mapdseval.hxx
@@ -0,0 +1,14 @@
+/*
+  Bojan Nikolic
+  $Id: mapdseval.hxx,v 1.1 2005/07/27 19:08:43 bnikolic Exp $
+
+  Interpolate a map to positions contained in a dataseries.
+*/
+#ifndef _ASTROMAP_MAPDSEVAL_HXX__
+#define _ASTROMAP_MAPDSEVAL_HXX__
+
+namespace AstroMap {
+
+
+}
+#endif
