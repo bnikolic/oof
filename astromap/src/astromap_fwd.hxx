bd6f8edf3c98b85916646eec71611df99087b993
bnikolic <>
2005-08-12 17:15:43 +0000

    *** empty log message ***

---
+++ astromap/src/astromap_fwd.hxx
@@ -0,0 +1,18 @@
+/*
+  Bojan Nikolic
+  $Id: astromap_fwd.hxx,v 1.1 2005/08/12 18:15:43 bnikolic Exp $
+
+  Collected useful astromap forwards 
+*/
+
+#ifndef __ASTROMAP_ASTROMAP_FWD_HXX__
+#define __ASTROMAP_ASTROMAP_FWD_HXX__
+
+namespace AstroMap {
+
+  class Map;
+  
+
+}
+
+#endif
