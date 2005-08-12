a8d2313f62fa96702ab61eed7de3093699a73fe6
bnikolic <>
2005-08-12 16:57:17 +0000

    *** empty log message ***

---
+++ oof/src/obsphasescreen.cxx
@@ -0,0 +1,14 @@
+/*
+  Bojan Nikolic
+  $Id: obsphasescreen.cxx,v 1.1 2005/08/12 17:57:17 bnikolic Exp $
+
+*/
+#include "obsphasescreen.hxx"
+
+namespace OOF {
+
+
+
+
+}
+
---
+++ oof/src/obsphasescreen.hxx
@@ -0,0 +1,17 @@
+/*
+  Bojan Nikolic
+  $Id: obsphasescreen.hxx,v 1.1 2005/08/12 17:57:17 bnikolic Exp $
+
+  An abstract class to represent a phase screen present during an
+  observed map, e.g., defocus or a pointing shift
+*/
+#ifndef _OOF_OBSPHASESCREEN_HXX__
+#define _OOF_OBSPHASESCREEN_HXX__
+
+namespace OOF {
+
+
+
+
+}
+#endif
