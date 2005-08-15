96501dec0edded862aedec5025acb64c702a6fbc
bnikolic <>
2005-08-15 14:45:44 +0000

    some progress  on obscompare but still not complete

--- oof/src/obscompare.cxx
+++ oof/src/obscompare.cxx
@@ -1,11 +1,53 @@
 /*
   Bojan Nikolic
-  $Id: obscompare.cxx,v 1.1 2005/08/12 19:32:16 bnikolic Exp $
+  $Id: obscompare.cxx,v 1.2 2005/08/15 15:45:44 bnikolic Exp $
 */
 
 #include "obscompare.hxx"
 
+#include <astromap.hxx>
+
+#include "obsphasescreen.hxx"
+#include "maptoresidual.hxx"
+#include "aperture/aperturemod.hxx"
+
+
 namespace OOF {
+
+  ObsCompare::ObsCompare( ApertureMod * aperture,
+			  AstroMap::Map &apsample ):
+    aperture(aperture),
+    ApScratch( AstroMap::Clone(apsample)),
+    ApScratchDephase( AstroMap::Clone(apsample))
+  {
+  }
+
+  ObsCompare::~ObsCompare(void)
+  {
+
+    delete aperture;
+    
+    delete ApScratch; 
+    delete ApScratchDephase;
+
+    for (size_t i =0 ; i < phasescreens.size() ; ++i )
+      {
+	delete phasescreens[i];
+	delete rescalculators[i];
+      }
+  }
+
+  
+  void ObsCompare::AddObs( MapToResidual * rc , ObsPhaseScreen * ps)
+  {
+    rescalculators.push_back(rc);
+    phasescreens.push_back(ps);
+  }
+
+  unsigned ObsCompare::NObs(void)
+  {
+    return phasescreens.size();
+  }
 
 
 }
--- oof/src/obscompare.hxx
+++ oof/src/obscompare.hxx
@@ -1,6 +1,6 @@
 /*
   Bojan Nikolic
-  $Id: obscompare.hxx,v 1.2 2005/08/15 14:11:39 bnikolic Exp $
+  $Id: obscompare.hxx,v 1.3 2005/08/15 15:45:45 bnikolic Exp $
 
   Compare models to observations!
 */
@@ -10,17 +10,58 @@
 
 #include <minimmodel.hxx>
 
+#include <astromap_fwd.hxx>
+#include <vector>
+
 namespace OOF {
 
-  class ObsCompare : public Minim::Minimisable {
+  // Forward declarations
+  class ApertureMod;
+  class ObsPhaseScreen;
+  class MapToResidual;
 
+  /*! This class compares a set of observations against predictions of
+   *  an aperture model and returns the residuals.  It also enumerates
+   *  the free parameters and hence allows for minimisations of the
+   *  residuals
+   */
+  class ObsCompare : public Minim::Minimisable {
+    
+    /*! This is the model for the aperture. This class will own the
+     * pointer. */
+    ApertureMod * aperture;
+    
+
+    /*! Each observation can have its own phase screen, typically this
+     *   is a defocus */
+    std::vector<ObsPhaseScreen *> phasescreens;
+
+    /*! These classes contain the actuall observed data and they
+     *  return the residuals  */
+    std::vector<MapToResidual *> rescalculators;
+
+    /// A scratch map for calculating apertures
+    AstroMap::Map *ApScratch;
+
+    /// A scratch map for calculating dephased apertures
+    AstroMap::Map * ApScratchDephase;
 
   public:
     
     // ------- Constructors / Destructors    -----------
+    ObsCompare( ApertureMod * aperture,
+		AstroMap::Map &apsample );		
 
+    virtual ~ObsCompare(void) ;
 
     // ------- Member functions -----------------------
+
+    /*! Add an observation to the current set. Note ownership of both
+     *  pointers will be taken. */
+    void AddObs( MapToResidual * rc , ObsPhaseScreen * ps);
+
+    /*! Return the number of observations in this set */
+    unsigned NObs(void);
 
 
     // ------- Methods inherited from minimisable
