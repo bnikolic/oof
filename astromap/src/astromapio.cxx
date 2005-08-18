6d2cc6f6948d00968dcc17a4b2a8e581fe11423d
bnikolic <>
2005-08-18 14:09:46 +0000

    added output of multiple maps to same fits file

--- astromap/src/astromapio.cxx
+++ astromap/src/astromapio.cxx
@@ -1,6 +1,6 @@
 /*
   Bojan Nikolic
-  $Id: astromapio.cxx,v 1.2 2005/08/18 14:33:55 bnikolic Exp $
+  $Id: astromapio.cxx,v 1.3 2005/08/18 15:09:46 bnikolic Exp $
 */
 
 #include "astromapio.hxx"
@@ -17,6 +17,26 @@
 
 namespace AstroMap {
 
+
+  void FitsWrite ( Map & m,
+		   BNFits::FitsF &fout)
+
+  {
+    using namespace BNFits;
+
+    std::vector<long> axes(2);
+    axes[0] = m.nx;
+    axes[1] = m.ny;
+    fout.MkImage(axes, DOUBLE_IMG);
+    
+    WriteImg( fout,
+	      m);
+    
+    FitsWrite( *(m.cs), fout,
+	       m.nx, m.ny);    
+
+  }
+
   void FitsWrite ( Map & m,
 		   const char * fnameout )
   {
@@ -24,17 +44,8 @@ namespace AstroMap {
 
     try {
       FitsF fout ( fnameout, FitsF::create);
+      FitsWrite( m, fout);
 
-      std::vector<long> axes(2);
-      axes[0] = m.nx;
-      axes[1] = m.ny;
-      fout.MkImage(axes, DOUBLE_IMG);
-
-      WriteImg( fout,
-		m);
-
-      FitsWrite( *(m.cs), fout,
-		 m.nx, m.ny);
     }     
     catch (FIOExc exc ) 
       {
@@ -43,6 +54,26 @@ namespace AstroMap {
 	std::cerr<<"\n Unkown results writen";
       }
     
+  }
+
+  void FitsWrite ( std::vector<Map *> mvect,
+		    const char * fnameout )
+  {
+    using namespace BNFits;
+
+    try {
+      FitsF fout ( fnameout, FitsF::create);
+      
+      for (unsigned i =0 ; i < mvect.size() ; ++i)
+	FitsWrite( (*mvect[i]), fout);
+    }     
+    catch (FIOExc exc ) 
+      {
+	std::cerr<<"\n ERROR: Writing image failed with following error:\n";
+	std::cerr<<exc.what();
+	std::cerr<<"\n Unkown results writen";
+      }
+
   }
 
 }
--- astromap/src/astromapio.hxx
+++ astromap/src/astromapio.hxx
@@ -1,12 +1,15 @@
 /*
   Bojan Nikolic
-  $Id: astromapio.hxx,v 1.1 2005/08/18 04:56:18 bnikolic Exp $
+  $Id: astromapio.hxx,v 1.2 2005/08/18 15:09:46 bnikolic Exp $
 
   Input/Output routines for astromap
 */
 #ifndef __ASTROMAP_ASTROMAPIO_HXX__
 #define __ASTROMAP_ASTROMAPIO_HXX__
 
+#include <bnfits_fwd.hxx>
+
+#include <vector>
 
 namespace AstroMap {
 
@@ -14,6 +17,12 @@ namespace AstroMap {
   class Map;
 
   void FitsWrite ( Map & m,
+		   BNFits::FitsF &fout);
+
+  void FitsWrite ( Map & m,
+		   const char * fnameout );
+
+  void FitsWrite ( std::vector<Map *> mvect,
 		   const char * fnameout );
   
 
