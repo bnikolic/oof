709e74ab40c44d16c3820f65e28c414832f5dc49
bnikolic <>
2005-08-26 20:08:06 +0000

    plotting directories is now functional

--- oof/bin/oofplot.py
+++ oof/bin/oofplot.py
@@ -1,5 +1,5 @@
 # Bojan Nikolic
-# $Id: oofplot.py,v 1.3 2005/08/21 21:46:53 bnikolic Exp $
+# $Id: oofplot.py,v 1.4 2005/08/26 21:08:06 bnikolic Exp $
 #
 # Various utilities for plotting OOF data
 
@@ -12,6 +12,39 @@ import pyplot
 import pyoof
 
 import oofreduce
+import oofcol
+
+def PlotDir(dirnamein, bbox=None):
+
+    "Make all the relevant plots"
+
+    obsdsfname=  oofcol.getpar(dirnamein,  "fitinfo.fits", "obsfilename")
+    npix= int(oofcol.getpar(dirnamein,  "fitinfo.fits", "npix") )
+    oversample= float( oofcol.getpar(dirnamein,  "fitinfo.fits", "oversample") )
+    fwhm=float(oofcol.getpar(dirnamein,  "fitinfo.fits", "ds_fwhm") )
+    extent=float(oofcol.getpar(dirnamein,  "fitinfo.fits", "ds_extent") )
+
+    PlotDSFile( obsdsfname,
+                os.path.join(dirnamein, "plots"),
+                prefix="obsbeam",
+                oversample= oversample,
+                npix=npix,
+                fwhm=fwhm,
+                extent=extent,
+                bbox=bbox)
+
+    PlotSimDrizzleBeams( obsdsfname,
+                         os.path.join(dirnamein, "fitbeams.fits"),
+                         os.path.join(dirnamein, "plots"),
+                         prefix="bestfitbeams",
+                         bbox=bbox,
+                         fwhm=fwhm,
+                         extent=extent)
+                         
+
+    
+                
+    
 
 def PlotDSFile( fnamein,
                 dirout,
@@ -19,7 +52,8 @@ def PlotDSFile( fnamein,
                 npix=128,
                 oversample=2.0,
                 fwhm=2.0,
-                extent=4.0
+                extent=4.0,
+                bbox=None
                ):
     "Plot beams contained in a dataseries file"
 
@@ -46,13 +80,15 @@ def PlotDSFile( fnamein,
         implot.plotmap( m,
                         fout=os.path.join(dirout,
                                           prefix + "-%i.png/PNG" % i ),
-                        colmap="heat")
+                        colmap="heat",
+                        bbox=bbox)
                                           
 
 def PlotSimDrizzleBeams(obsfilename, beamfilename,
                         dirout,
                         prefix="simdrizbeam",
-                        fwhm=2, extent=4):
+                        fwhm=1.0, extent=2.0,
+                        bbox=None):
 
     ds = oofreduce.SimBeamDS(obsfilename, beamfilename)
     print ds
@@ -64,7 +100,8 @@ def PlotSimDrizzleBeams(obsfilename, beamfilename,
         implot.plotmap(skym,
                        fout=os.path.join(dirout,
                                          prefix + "-%i.png/PNG" % i ),
-                       colmap="heat"  )
+                       colmap="heat",
+                       bbox=bbox)
 
 
 
