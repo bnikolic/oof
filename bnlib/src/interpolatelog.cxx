88f63354e4e502154b81cc24b9aecb4924d6e707
bnikolic <>
2005-08-29 20:02:05 +0000

    completed the implementation

--- bnlib/src/interpolatelog.cxx
+++ bnlib/src/interpolatelog.cxx
@@ -1,15 +1,51 @@
 /*
   Bojan Nikolic
-  $Id: interpolatelog.cxx,v 1.1 2005/08/29 20:41:52 bnikolic Exp $
+  $Id: interpolatelog.cxx,v 1.2 2005/08/29 21:02:05 bnikolic Exp $
 */
 
 #include "interpolatelog.hxx"
 
+#include <valarray>
+#include <cmath>
+
+#include "interpolategsl.hxx"
+
+
 namespace BNLib {
 
-  
+  InterpolateLog::InterpolateLog( double * xvals , double * yvals , size_t size ,
+				  imethod logmethod, imethod realmethod):
+    InterpolatorBase( xvals, yvals, size, logmethod)
+  {
+
+    realspace = new InterpolatorGSL( xvals, yvals, size , realmethod);
+    
+    std::valarray<double> dcopy ( yvals, size);
+
+    std::valarray<double> dlog = std::log(dcopy);
+
+    logspace =  new InterpolatorGSL( xvals, 
+				     &dlog[0], size , 
+				     logmethod);
 
 
+  }
+
+  InterpolateLog::~InterpolateLog()
+  {
+    delete logspace;
+    delete realspace;
+  }
+
+  double InterpolateLog::operator() (double x) 
+  {
+    return exp(  (*logspace)(x) );
+  }
+
+  double InterpolateLog::Idx(double xmin, double xmax) 
+  {
+    return realspace->Idx(xmin, xmax);
+  }
 
 }
 
--- bnlib/src/interpolatelog.hxx
+++ bnlib/src/interpolatelog.hxx
@@ -1,13 +1,52 @@
 /*
   Bojan Nikolic
-  $Id: interpolatelog.hxx,v 1.1 2005/08/29 20:41:52 bnikolic Exp $
+  $Id: interpolatelog.hxx,v 1.2 2005/08/29 21:02:05 bnikolic Exp $
 
   Routines to interpolate in log-space
 */
 #ifndef BNLIB_INTERPOLATE_LOG__
 #define BNLIB_INTERPOLATE_LOG__
 
+#include "interpolate.hxx"
+
 namespace BNLib {
+
+  /*! Does the interpolation in log space. Currently implemented using
+   *  InterpolatorGSL.
+   */
+  class InterpolateLog : public InterpolatorBase {
+    
+    InterpolatorBase * logspace;
+
+    /*! realspace has the real space data -- use this to do the
+     * integration */
+    InterpolatorBase * realspace;
+
+  public:
+
+    /*! Log space interpolation. Two methods are supplied: logmethod
+     *  is the method to use when evaluting the function at particular
+     *  points -- this is the main aim of the class and actuall
+     *  interpolation will be done in log space. 
+     *  
+     *  In order to support integration of the data a real space
+     *  version of data is retained. The interpolation method used for
+     *  this is realmethod. Setting this to linear should avoid any
+     *  numerical problems.
+     * 
+     **/
+    InterpolateLog( double * xvals , double * yvals , size_t size ,
+		    imethod logmethod, imethod realmethod);
+
+    virtual ~InterpolateLog();
+
+    /* ----------- Inherited from InterpolatorBase -------------*/
+
+    virtual double operator() (double x)  ;
+    virtual double Idx(double xmin, double xmax) ;
+    
+
+  };
 
   
 
