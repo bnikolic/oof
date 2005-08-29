fb5f6983c6b2ca4aa25da96a582f349e93c99853
bnikolic <>
2005-08-29 19:41:52 +0000

    *** empty log message ***

---
+++ bnlib/src/interpolatelog.cxx
@@ -0,0 +1,16 @@
+/*
+  Bojan Nikolic
+  $Id: interpolatelog.cxx,v 1.1 2005/08/29 20:41:52 bnikolic Exp $
+*/
+
+#include "interpolatelog.hxx"
+
+namespace BNLib {
+
+  
+
+
+
+}
+
+
---
+++ bnlib/src/interpolatelog.hxx
@@ -0,0 +1,18 @@
+/*
+  Bojan Nikolic
+  $Id: interpolatelog.hxx,v 1.1 2005/08/29 20:41:52 bnikolic Exp $
+
+  Routines to interpolate in log-space
+*/
+#ifndef BNLIB_INTERPOLATE_LOG__
+#define BNLIB_INTERPOLATE_LOG__
+
+namespace BNLib {
+
+  
+
+
+
+}
+
+#endif
