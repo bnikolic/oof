eac14427cff151f84d3a8dfb9c6a3fe7e12ab20b
bnikolic <>
2005-09-14 15:08:22 +0000

    *** empty log message ***

---
+++ bnlib/src/integrateutils.cxx
@@ -0,0 +1,16 @@
+/*
+ * Bojan Nikolic
+ *
+ * $Id: integrateutils.cxx,v 1.1 2005/09/14 16:08:23 bnikolic Exp $
+ */
+
+#include "integrateutils.hxx"
+
+namespace BNLib {
+
+
+
+
+}
+
+
---
+++ bnlib/src/integrateutils.hxx
@@ -0,0 +1,19 @@
+/*
+ * Bojan Nikolic
+ *
+ * Some utilities to deal with difficult integrations.
+ * 
+ * $Id: integrateutils.hxx,v 1.1 2005/09/14 16:08:23 bnikolic Exp $
+ */
+
+#ifndef _BNLIB_INTEGRATEUTILS_HXX__
+#define _BNLIB_INTEGRATEUTILS_HXX__
+
+namespace BNLib {
+
+
+
+
+}
+
+#endif
