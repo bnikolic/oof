ace77c2809576056f2ff97dfced9831c28e84091
bnikolic <>
2005-09-14 16:40:16 +0000

    completed the integreateutils

--- bnlib/src/integrateutils.cxx
+++ bnlib/src/integrateutils.cxx
@@ -1,12 +1,50 @@
 /*
  * Bojan Nikolic
  *
- * $Id: integrateutils.cxx,v 1.1 2005/09/14 16:08:23 bnikolic Exp $
+ * $Id: integrateutils.cxx,v 1.2 2005/09/14 17:40:16 bnikolic Exp $
  */
 
 #include "integrateutils.hxx"
 
+#include <cmath>
+
+#include "unaryfn.hxx"
+#include "integrate.hxx"
+#include "linraster.hxx"
+
+
 namespace BNLib {
+
+  double SectionInteg (UnaryDD &fn , double epsabs, 
+		       double epsrel, unsigned maxsteps,
+		       std::vector<double> &bounds )
+  {
+
+    double result = 0 ;
+
+    AdaptiveInt integrator (  fn,  epsabs, epsrel,  maxsteps );
+
+    for (unsigned i =0 ; i < bounds.size() - 1 ; ++i )
+      {
+	result += integrator.Eval( bounds[i] , bounds[i+1] );
+      }
+
+    return result;
+  }
+
+  double LogSecInteg ( UnaryDD &fn , double xmin, double xmax, double step , 
+		       double epsabs, 
+		       double epsrel, unsigned maxsteps )
+  {
+    
+    std::vector<double> bounds ( (int)(log(xmax/xmin) / log(step) ) +1 );
+    LogRaster( bounds.begin() , bounds.begin() -1 , 
+	       xmin, xmax);
+    bounds[bounds.size() -1 ] = xmax;
+
+    return SectionInteg( fn  , epsabs, epsrel, maxsteps, bounds);
+    
+  }
 
 
 
--- bnlib/src/integrateutils.hxx
+++ bnlib/src/integrateutils.hxx
@@ -3,15 +3,38 @@
  *
  * Some utilities to deal with difficult integrations.
  * 
- * $Id: integrateutils.hxx,v 1.1 2005/09/14 16:08:23 bnikolic Exp $
+ * $Id: integrateutils.hxx,v 1.2 2005/09/14 17:40:16 bnikolic Exp $
  */
 
 #ifndef _BNLIB_INTEGRATEUTILS_HXX__
 #define _BNLIB_INTEGRATEUTILS_HXX__
 
+#include <vector>
+
 namespace BNLib {
 
+  // Forward declarations
+  class UnaryDD;
 
+
+  /*! Integrate a function section by section in order to force a more
+   *  accurate result. By customising sections can focus ranges with
+   *  known difficult features. Uses adaptive integrator. bounds[0] is
+   *  xmin, bounds[bounds.size-1] is xmax.
+   */
+  double SectionInteg (UnaryDD &fn , double epsabs, 
+		       double epsrel, unsigned maxsteps,
+		       std::vector<double> &bounds );
+
+
+  /*! Evaluate the integral from xmin to xmax by subdividing the range
+   *  into equal ranges in log-space with ratio step.
+   */
+  double LogSecInteg ( UnaryDD &fn , double xmin, double xmax, double step , 
+		       double epsabs, 
+		       double epsrel, unsigned maxsteps );
+  
+  
 
 
 }
