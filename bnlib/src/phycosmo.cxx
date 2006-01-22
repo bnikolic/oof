77c107a4091a765b773450fca4771edb0899e286
bnikolic <>
2006-01-22 20:43:54 +0000

    started adding cosmology functions

---
+++ bnlib/src/phycosmo.cxx
@@ -0,0 +1,16 @@
+/*
+  Bojan Nikolic
+  $Id: phycosmo.cxx,v 1.1 2006/01/22 20:43:54 bnikolic Exp $
+
+*/
+
+#include "phycosmo.hxx"
+
+namespace BNLib{
+
+  namespace Phy {
+
+  }
+}
+
+
---
+++ bnlib/src/phycosmo.hxx
@@ -0,0 +1,18 @@
+/*
+  Bojan Nikolic
+  $Id: phycosmo.hxx,v 1.1 2006/01/22 20:43:54 bnikolic Exp $
+
+  Some simple cosmology routines
+*/
+
+#ifndef _BNLIB_PHYCONSTANTS_HXX__
+#define _BNLIB_PHYCONSTANTS_HXX__
+
+namespace BNLib{
+
+  namespace Phy {
+
+  }
+}
+
+#endif
