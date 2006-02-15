bb9c624055e277b56509f5903d6155f8c0804f4e
bnikolic <>
2006-02-15 23:41:00 +0000

    *** empty log message ***

--- astromap/src/convolve.cxx
+++ astromap/src/convolve.cxx
@@ -1,13 +1,59 @@
 /*
   Bojan Nikolic
-  $Id: convolve.cxx,v 1.1 2006/02/15 23:23:36 bnikolic Exp $
+  $Id: convolve.cxx,v 1.2 2006/02/15 23:41:00 bnikolic Exp $
 
 */
 
 #include "convolve.hxx"
 
+#include <stdexcept>
+
+#include "astromap.hxx"
+#include "fft.hxx"
+
 
 namespace AstroMap {
+
+  Map * FFTConvolve( const Map &  m1,
+		     const Map &  m2)
+  {
+    if (not (m1.nx == m2.nx &&
+	     m1.ny == m2.ny) )
+      throw std::runtime_error( "Don't know how to convolve maps without identical dimensions");
+
+    Map m1amp ( m1);
+
+    Map m1phase (m1);
+    m1phase *= 0;
+
+    Map m2amp ( m2);
+
+    Map m2phase (m2);
+    m2phase *= 0;
+
+    
+    FFTFact InverseFF ( m1.nx, m1.ny, 
+			FFTFact::backward,
+			FFTFact::dontcenter);
+
+    InverseFF.fftamphi( m1amp, m1phase);
+    InverseFF.fftamphi( m2amp, m2phase);
+
+    m1amp *= m2amp;
+    m1phase += m2phase;
+
+
+    FFTFact ForwardFF ( m1.nx, m1.ny, 
+			FFTFact::forward,
+			FFTFact::center);
+
+    ForwardFF.fftamphi( m1amp, m1phase);
+    m1amp /= ( m1amp.nx * m1amp.ny);
+    
+    return Clone(m1amp);
+
+
+  }
 
 
 }
--- astromap/src/convolve.hxx
+++ astromap/src/convolve.hxx
@@ -1,6 +1,6 @@
 /*
   Bojan Nikolic
-  $Id: convolve.hxx,v 1.1 2006/02/15 23:23:36 bnikolic Exp $
+  $Id: convolve.hxx,v 1.2 2006/02/15 23:41:00 bnikolic Exp $
 
   Tools for convolving maps together
 
@@ -8,7 +8,21 @@
 #ifndef _ASTROMAP_CONVOLVE_HXX__
 #define _ASTROMAP_CONVOLVE_HXX__
 
+#include "astromap_fwd.hxx"
+
+
 namespace AstroMap {
+
+  /*   -----  FFT Convolution -------------
+
+  There is a 33% time advantage of storing one of the FFT'd maps if
+  that map does not change.
+
+  */
+
+  /* Convolve maps m1 and m2 using the FFT */
+  Map * FFTConvolve( const Map &  m1,
+		     const Map &  m2);
 
 
 }
