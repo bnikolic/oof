256cd9a7a56292e3b1a686bb3a4c4d28f6346538
bnikolic <>
2006-02-18 21:51:27 +0000

    *** empty log message ***

--- oofanalysis/gbt/modtherm01.py
+++ oofanalysis/gbt/modtherm01.py
@@ -1,12 +1,13 @@
 # Bojan Nikolic
-# $Id: modtherm01.py,v 1.2 2006/02/18 20:40:02 bnikolic Exp $
+# $Id: modtherm01.py,v 1.3 2006/02/18 21:51:19 bnikolic Exp $
 #
 # Investigate thermal effects
 
 import os
-
+import pyfits
 import oofplot
 import pyplot
+import oofcol
 
 def PlotSfcs():
 
@@ -35,6 +36,33 @@ def MRMS( sdate, sno):
                                    "plots/measureamp.eps/CPS")
 
     print pyplot.MapRMS(phasef, ilumf)
+
+scanlist =     [ 22,   52, 61, 70, 79, 88, 97, 106, 115, 124, 133, 142, 151, 160]
+
+scanlist =     [ 22,   52, 61, 70,  106, 115, 124, 133, 142, 151, 160]
+
+def PrintPars(projname, zmax=5):
+
+    dirin = "oofout%s" % projname
+    pointd= pyfits.open("thermdata/pointing.fits")[1].data
+    
+
+    def GetZP(parname):
+        return oofcol.getpar(thisdir,
+                      "offsetpars.fits",
+                      parname,
+                      parcol="ParName",
+                      valcol="ParValue")
+
+    for s in scanlist:
+        thisdir= os.path.join(dirin, "s%i-l-db-000/z%i" % (s, zmax))
+        print "S:  %i \t  z3 :  %g \t z5  %g"   %  ( s,
+                                                     GetZP("z3"),
+                                                     GetZP("z5"))
+
+        
+
+        
     
     
 
