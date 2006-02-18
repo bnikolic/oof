bf98ee9477250dfc38d1afa41f05fdaca94bb4a1
bnikolic <>
2006-02-18 20:40:02 +0000

    *** empty log message ***

--- oofanalysis/gbt/modtherm01.py
+++ oofanalysis/gbt/modtherm01.py
@@ -1,7 +1,41 @@
 # Bojan Nikolic
-# $Id: modtherm01.py,v 1.1 2005/12/07 13:05:06 bnikolic Exp $
+# $Id: modtherm01.py,v 1.2 2006/02/18 20:40:02 bnikolic Exp $
 #
 # Investigate thermal effects
 
+import os
+
+import oofplot
+import pyplot
+
+def PlotSfcs():
+
+    def plot ( sdate, sno ):
+
+        zfin = "oofout%s/s%i-l-db-000/z5/offsetpars.fits" % ( sdate, sno )
+        oofplot.PlotZernFile( zfin,
+                              "plots/sfc%s-%i.eps/CPS" % (sdate, sno) )
+        oofplot.PlotZernFile( zfin,
+                              "plots/sfc%s-%i.png/PNG" % (sdate, sno) )
+
+
+    for sno in [ 83 , 91, 135, 143, 187, 195]:
+        plot ("1130" , sno)
+        plot ("1130" , sno)
+
+def MRMS( sdate, sno):
+
+    indir= "oofout%s/s%i-l-db-000/z5" % (sdate, sno)
+    
+    phasef= oofplot.PlotZernFile( os.path.join(indir ,
+                                               "fitpars.fits"),
+                                  "plots/measuresfc.eps/CPS")
+    ilumf = oofplot.PlotIllumFile( os.path.join(indir ,
+                                                "fitpars.fits"),
+                                   "plots/measureamp.eps/CPS")
+
+    print pyplot.MapRMS(phasef, ilumf)
+    
+    
 
 
--- oofanalysis/gbt/red2005.py
+++ oofanalysis/gbt/red2005.py
@@ -1,5 +1,5 @@
 # Bojan Nikolic
-# $Id: red2005.py,v 1.1 2005/09/17 22:15:30 bnikolic Exp $
+# $Id: red2005.py,v 1.2 2006/02/18 20:40:02 bnikolic Exp $
 #
 # The general routine for reducing data from the 2005 campaigns
 
@@ -7,8 +7,10 @@ import os
 import re
 
 import oofreduce
+import oofplot
 
-datadir="/home/bnikolic/data/gbtoof/ver001/"
+datadir=os.path.join(os.environ["AstroData"],
+                     "gbtoof/ver001/")
 
 def GetScanList(obsdate):
 
@@ -18,6 +20,18 @@ def GetScanList(obsdate):
     fnre=re.compile(r"^s(?P<sno>\d+)-l-db\.fits$" )
 
     for fname in os.listdir( os.path.join(datadir, obsdate)):
+        m=fnre.match(fname)
+        if m : res.append( int(m.group("sno")) )
+    return res
+
+def GetRedScanList(obsdate):
+
+    "Get the list of all reduced oof scans for this date"
+
+    res=[]
+    fnre=re.compile(r"^s(?P<sno>\d+)-l-db-000$" )
+
+    for fname in os.listdir( os.path.join("oofout%s"% obsdate)):
         m=fnre.match(fname)
         if m : res.append( int(m.group("sno")) )
     return res
@@ -38,7 +52,19 @@ def Red(obsdate):
                       prefdirout="oofout" +obsdate )
         
     
+def Plot(obsdate):
 
+    for sno in GetRedScanList(obsdate):
+
+            oofplot.PlotDir( "oofout%s/s%i-l-db-000/z5" % (obsdate, sno) ,
+                     bbox=[-0.6e-3, 0.4e-3, -0.5e-3, 0.5e-3],
+                     npix=512,
+                     fwhm=4, extent=10,
+                     ncont=5,
+                     hardcopy=True)
+
+        
+    
         
         
 
