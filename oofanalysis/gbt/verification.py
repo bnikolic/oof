bc8d6965689d385ad02d5bb95021b3c20d7dc489
bnikolic <>
2006-11-20 23:31:20 +0000

    *** empty log message ***

--- oofanalysis/gbt/verification.py
+++ oofanalysis/gbt/verification.py
@@ -1,5 +1,5 @@
 # Bojan Nikolic
-# $Id: verification.py,v 1.3 2006/04/25 15:10:07 bnikolic Exp $
+# $Id: verification.py,v 1.4 2006/11/20 23:31:20 bnikolic Exp $
 #
 # Plot OOF verification results
 
@@ -26,19 +26,29 @@ def ParseData(fin):
             pol= ld[2]
             src = ld[0]
             elev=float(ld[3])
+            uttime=float(ld[4])
 
             apeff= float(ld[8])
             apefferr= float(ld[10])
             model   = ld[11]
 
-            if model == "(None)": model = 0
-            elif model == "(2005WinterV1)" : model =1
+            if model == "(None)":
+                model = 0
+            elif model == "(2005SpringV1)" :
+                model = "2005SpringV1" 
+            elif model == "(2005WinterV1)" :
+                model = "2005WinterV1"
 
-            res.append ( ( pol, elev,( apeff, apefferr ) , model , src))
+            res.append ( ( pol, elev,( apeff, apefferr ) , model , src, uttime))
 
     return res
 
-def PlotGain( d):
+def PlotGain( d,
+              model,
+              utmin=0,
+              utmax=24,
+              mineff=0.2,
+              maxeff=0.5):
 
     def ProcD(d2):
 
@@ -57,8 +67,9 @@ def PlotGain( d):
         return pt
         
 
-    rcpnone = [ x for x in d if x[0]=="RCP" and x[3]==0 ]
-    rcpv1 = [ x for x in d if x[0]=="RCP" and x[3]==1 ]    
+    rcpnone = [ x for x in d if x[0]=="RCP" and x[3]==0 and x[5] > utmin and x[5] < utmax ]
+    rcpv1 = [ x for x in d if x[0]=="RCP" and x[3]==model and x[5] > utmin and x[5] < utmax ]
+
 
     pt      = [ pyxplot.graph.data.list( rcpnone, x=1, y=2, dy=3),
                 pyxplot.graph.data.list( rcpv1, x=1, y=2, dy=3),                
@@ -67,10 +78,10 @@ def PlotGain( d):
     c = canvas.canvas()
     
     gon = c.insert(pyxplot.graph.graphxy(width=pyxplot.MNRAS_SC,
-                                          x=pyxplot.axis(r"$\theta\,$(degrees)",
+                                          x=pyxplot.axis(r"$E\,$(degrees)",
                                                          xmin=0, xmax=90) ,
                                           y=pyxplot.axis(r"$\eta_{\rm a}$",
-                                                         xmin=0.2, xmax=0.5),
+                                                         xmin=mineff, xmax=maxeff),
                                           ))
     gon.plot(ProcD(rcpv1),
              [ graph.style.symbol(graph.style.symbol.changesquare,
@@ -84,7 +95,7 @@ def PlotGain( d):
                                            ypos =gon.height+0.5,
                                            x=gon.axes["x"].createlinkaxis(),
                                           y=pyxplot.axis(r"$\eta_{\rm a}$",
-                                                         xmin=0.2, xmax=0.5),
+                                                         xmin=mineff, xmax=maxeff),
                                            ))
 
 
@@ -99,4 +110,5 @@ def PlotGain( d):
 
 
 
-        
+#d=ParseData(open("cdata/efficiency050411.txt"))        
+#PlotGain(d , "2005SpringV1", 3, 12.5, 0.25, 0.55)
