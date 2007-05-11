473bae5bcdc5c4cad43577369518ae5d89797f45
Bojan Nikolic <bojan@bnikolic.co.uk>
2007-05-11 15:36:41 +0100

    Implementation of the corners from Kolmogorov platform

--- bnlib/src/kolmogorov.cxx
+++ bnlib/src/kolmogorov.cxx
@@ -9,5 +9,37 @@
 
 namespace BNLib {
 
+  /** Generate the four corner samples of the Kolmogorov
+      platform. Will use six random samples from the normvect.
+      
+   */
+  void KPCorners( double * alpha,
+		  double * beta,
+		  double * gamma,
+		  double * delta,
+		  const double * normvect)
+  {
+    const double SigmaC=0.86638145132179845;
+    const double SigmaD=3.2798677905095714;
+
+    const double Rad = normvect[4] * SigmaD;
+    const double Rbg = normvect[5] * SigmaD;
+
+
+    *alpha = normvect[0] * SigmaC + 0.5 * Rad;
+    *beta  = normvect[1] * SigmaC + 0.5 * Rbg;
+    *gamma = normvect[2] * SigmaC - 0.5 * Rbg;
+    *delta = normvect[3] * SigmaC - 0.5 * Rad;
+  }
+
+  
+
+  void KolmogorovPlatform( size_t N,
+			   double * grid,
+			   const double * normvect)
+  {
+
+  }
+
 
 }
