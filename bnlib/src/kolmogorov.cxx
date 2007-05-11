c3104654542c3e93624772437def98c232fb292a
Bojan Nikolic <bojan@bnikolic.co.uk>
2007-05-11 14:48:16 +0100

    Begin work on generation of Kolmogorov spectra

---
+++ bnlib/src/kolmogorov.cxx
@@ -0,0 +1,13 @@
+/**
+   \file kolmogorov.cxx
+
+   Boan Nikolic <bn204@mrao.cam.ac.uk>, <bojan@bnikolic.co.uk>
+   May 2007 
+*/
+
+#include "kolmogorov.hxx"
+
+namespace BNLib {
+
+
+}
---
+++ bnlib/src/kolmogorov.hxx
@@ -0,0 +1,37 @@
+/**
+
+   \file kolmogorov.hxx
+
+   Boan Nikolic <bn204@mrao.cam.ac.uk>, <bojan@bnikolic.co.uk>
+   May 2007 
+
+   Construct 2D radnom fields with Kolmorogov structure
+
+*/
+#ifndef _BNLIB_KOLMOGOROV_HXX__
+#define _BNLIB_KOLMOGOROV_HXX__
+
+#include <stdlib.h>
+
+namespace BNLib {
+
+  /**
+     Construct a Kolmogorov random field on a NxN grid using the
+     algorithm of Lane et al 2002 (1992WRM.....2..209L).
+     
+     \param N size of the grid; must be 2^n+1
+
+     \param grid pre-allocated grid of size NxN
+     
+     \param normvect a vector of std normal distributed random samples
+     of length NxN+2
+     
+  */
+  void KolmogorovPlatform( size_t N,
+			   double * grid,
+			   const double * normvect);
+  
+}
+#endif
+
+   
