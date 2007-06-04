19a8f8d20c3b22f84bc233890950517010a19b51
Bojan Nikolic <bojan@bnikolic.co.uk>
2007-06-04 17:49:08 +0100

    First illustration for visualisation of 3D turbulence generation

--- bnlib/pybind/test/threedimturbulence.py
+++ bnlib/pybind/test/threedimturbulence.py
@@ -7,46 +7,66 @@
 import vtk
 from vtk.util.colors import *
 
-CubeModel = vtk.vtkCubeSource()
-CubeModel.SetCenter(.5, .5, .5)
-Edges = vtk.vtkExtractEdges()
-Edges.SetInputConnection(CubeModel.GetOutputPort())
-Tubes = vtk.vtkTubeFilter()
-Tubes.SetInputConnection(Edges.GetOutputPort())
-Tubes.SetRadius(.01)
-Tubes.SetNumberOfSides(6)
-Tubes.UseDefaultNormalOn()
-Tubes.SetDefaultNormal(.577, .577, .577)
-# Create the mapper and actor to display the cube edges.
-TubeMapper = vtk.vtkPolyDataMapper()
-TubeMapper.SetInputConnection(Tubes.GetOutputPort())
-CubeEdges = vtk.vtkActor()
-CubeEdges.SetMapper(TubeMapper)
-CubeEdges.GetProperty().SetDiffuseColor(khaki)
-CubeEdges.GetProperty().SetSpecular(.4)
-CubeEdges.GetProperty().SetSpecularPower(10)
+def MkCube( cx, cy, cz, l ):
+    CubeModel = vtk.vtkCubeSource()
+    CubeModel.SetCenter(cx, cy ,cz)
+    CubeModel.SetXLength(l)
+    CubeModel.SetYLength(l)
+    CubeModel.SetZLength(l)
+    return CubeModel
+
+def Edges(cm,
+          cl=khaki):
+
+    Edges = vtk.vtkExtractEdges()
+    Edges.SetInputConnection(cm.GetOutputPort())
+    Tubes = vtk.vtkTubeFilter()
+    Tubes.SetInputConnection(Edges.GetOutputPort())
+    Tubes.SetRadius(.01)
+    Tubes.SetNumberOfSides(6)
+    Tubes.UseDefaultNormalOn()
+    Tubes.SetDefaultNormal(.577, .577, .577)
+    # Create the mapper and actor to display the cube edges.
+    TubeMapper = vtk.vtkPolyDataMapper()
+    TubeMapper.SetInputConnection(Tubes.GetOutputPort())
+    CubeEdges = vtk.vtkActor()
+    CubeEdges.SetMapper(TubeMapper)
+    CubeEdges.GetProperty().SetDiffuseColor(cl)
+    CubeEdges.GetProperty().SetSpecular(.4)
+    CubeEdges.GetProperty().SetSpecularPower(10)
+
+    return CubeEdges
 
 
+def Render( al ):
 
+    ren = vtk.vtkRenderer()
+    renWin = vtk.vtkRenderWindow()
+    renWin.AddRenderer(ren)
+    renWin.SetSize(640, 480)
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(renWin)
 
-ren = vtk.vtkRenderer()
-renWin = vtk.vtkRenderWindow()
-renWin.AddRenderer(ren)
-renWin.SetSize(640, 480)
-iren = vtk.vtkRenderWindowInteractor()
-iren.SetRenderWindow(renWin)
+    for a in al:
+        ren.AddActor(a)
 
-ren.AddActor(CubeEdges)
+    ren.SetBackground(slate_grey)
 
-ren.SetBackground(slate_grey)
+    ren.ResetCamera()
+    ren.GetActiveCamera().Dolly(1.2)
+    ren.GetActiveCamera().Azimuth(30)
+    ren.GetActiveCamera().Elevation(20)
+    ren.ResetCameraClippingRange()
 
-ren.ResetCamera()
-ren.GetActiveCamera().Dolly(1.2)
-ren.GetActiveCamera().Azimuth(30)
-ren.GetActiveCamera().Elevation(20)
-ren.ResetCameraClippingRange()
+    iren.Initialize()
+    renWin.Render()
+    iren.Start()
 
-iren.Initialize()
-renWin.Render()
-iren.Start()
+def Il1():
 
+    c1=Edges(MkCube(0.5,0.5,0.5, 1))
+    c3=Edges(MkCube(0.25,0.25,0.25, 0.5),cl=blue)
+    c4=Edges(MkCube(0.365,0.365,0.365, 0.25), cl=red)
+    Render( [c1,c3,c4])
+    
+    
