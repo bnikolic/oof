c82b9abdec4d13126ff36093a48980c6dd97292d
Bojan Nikolic <bojan@bnikolic.co.uk>
2007-06-18 11:38:51 +0100

    Small improvements to the testing routines for 3d turbulence

--- bnlib/pybind/test/kolmogorov3d.py
+++ bnlib/pybind/test/kolmogorov3d.py
@@ -6,20 +6,36 @@
 
 import sys; sys.path.extend(["/import/appcs/bn204/p/bnprog-devel-main/"+x for x in ["bin", "lib"] ])
 
+from setup import *
+
 import pybnlib
 import numarray
+import numpy
 from  matplotlib import pylab
 
 
 
 
-def GenK(N):
+def GenK(N,usenumpy=False):
     grid=pybnlib.doubleArray( N*N*N)
     rfn=pybnlib.NormDistZM(1.0)
     pybnlib.Kolmogorov3D( grid, N, rfn)
-    res=numarray.zeros( (N,N,N), numarray.Float64)
+    if usenumpy:
+        res=numpy.zeros( (N,N,N), numpy.float64)
+    else:
+        res=numarray.zeros( (N,N,N), numarray.Float64)
+        
     for k in range(N):
         for j in range(N):
             for i in range(N):
                 res[i,j,k] = grid[ k*N*N+j*N+i]
     return res
+
+def Animate(g):
+    for i in range(1,64,5):
+        pylab.clf()
+        x= g[0:i,:,:]
+        y= numarray.sum(x, axis=0)
+        pylab.matshow( y)
+        pylab.savefig("temp/3dturb-%03i.png" % i)
+        
