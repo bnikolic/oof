77f710da7e44933c8eceff8f4bb008b27777f6ef
Bojan Nikolic <bojan@bnikolic.co.uk>
2007-06-26 17:42:41 +0100

    Add module for python helper scripts

---
+++ bnlib/pybind/bin/kolmogorovutils.py
@@ -0,0 +1,17 @@
+# Bojan Nikolic <bn204@mrao.cam.ac.uk>, <bojan@bnikolic.co.uk>
+#
+# June 2007
+#
+# Utilities to support the use of Kolmogorov utilities in Python
+
+
+import pybnlib
+
+def GenerateKolmogorov3D( Nx,Ny,Nz):
+
+    grid=pybnlib.doubleArray( Nx*Ny*Nz)
+    rfn=pybnlib.NormDistZM(1.0)
+    pybnlib.Kolmogorov3D( grid, Nx,Ny,Nz, rfn)
+    return grid
+
+
