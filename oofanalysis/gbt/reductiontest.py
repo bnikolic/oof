9b8fc80b49694215f778475d83dd568e0ae373bc
Bojan Nikolic <bojan@bnikolic.co.uk>
2007-10-01 17:56:33 +0100

    * oof: add simple reduction script

---
+++ oofanalysis/gbt/reductiontest.py
@@ -0,0 +1,11 @@
+# Bojan Nikolic <bojan@bnikolic.co.uk>
+# 2007
+#
+# Test the reduction runs finely
+
+from setup import *
+
+import oofreduce
+import oofplot
+
+sample="/home/bn204/d/data/oof/gbt/ver001/0411/s114-l-db.fits"
