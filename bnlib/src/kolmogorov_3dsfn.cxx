0ac39d1d599bdf61d3312fc2d5a36068ad736050
Bojan Nikolic <bnikolic@bnikolic-laptop>
2007-11-19 18:31:48 +0000

    Start on the direct structure function measurement

---
+++ bnlib/src/kolmogorov_3dsfn.cxx
@@ -0,0 +1,15 @@
+/**
+   \file kolmogorov_3dsfn.cxx
+   
+   Bojan Nikolic <bn204@mrao.cam.ac.uk>, <bojan@bnikolic.co.uk> 
+
+   Initial version November 2007
+*/
+
+#include "kolmogorov_3dsfn.hxx"
+
+namespace BNLib {
+
+
+}
+
---
+++ bnlib/src/kolmogorov_3dsfn.hxx
@@ -0,0 +1,20 @@
+/**
+   \file kolmogorov_3dsfn.hxx
+   
+   Bojan Nikolic <bn204@mrao.cam.ac.uk>, <bojan@bnikolic.co.uk> 
+
+   Initial version November 2007
+
+   Measure structure functions of three dimensional cubes directly
+   using random sampling. See also 2d versions in astromap.
+*/
+#ifndef __BNLIB__KOL_3DSFN_HXX__
+#define __BNLIB__KOL_3DSFN_HXX__
+
+namespace BNLib {
+
+
+}
+
+
+#endif
