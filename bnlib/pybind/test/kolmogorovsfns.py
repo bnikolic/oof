68c5b7f3a6e43561721a896f7760bc338b85e0c5
Bojan Nikolic <bnikolic@bnikolic-laptop>
2007-11-20 10:55:31 +0000

    bnlib/kolomogorov : improved plotting of structure functions

--- bnlib/pybind/test/kolmogorovsfns.py
+++ bnlib/pybind/test/kolmogorovsfns.py
@@ -16,17 +16,18 @@ import kolmogorovutils
 
 def PlotSFN(r,
             fnameout,
-            fid=129):
+            fid):
 
-    mask = r[:,0] > 0
+    mask = numarray.logical_and( r[:,0] > 0,
+                                 r[:,1] > 0)
     xvals= numarray.arange(len(r))
-    yvals= r[:,0]/r[:,1]
+    yvals= (r[:,0])[mask]/(r[:,1])[mask]
 
     m2= (xvals > 0)
-    yfid= 6.88* (xvals[m2]/129.0)**(2.0/3)
+    yfid= 6.88* (xvals[m2]/float(fid))**(2.0/3)
 
     pyxplot.vect( [xvals[mask], xvals[m2] ],
-                  [yvals[mask], yfid      ],
+                  [yvals, yfid      ],
                   fnameout,
                   xax=pyxplot.axis(r"$d$", type="log"),
                   yax=pyxplot.axis(r"$D(d)$", type="log"),
