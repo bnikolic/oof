0a7dba28726fe59ae84446712fddda7ed32e5f70
Bojan Nikolic <bnikolic@bnikolic-laptop>
2007-11-20 17:29:34 +0000

    *bnib/kolmogorov : Tests for balanced edge iters

--- bnlib/pybind/test/test_k3changes.py
+++ bnlib/pybind/test/test_k3changes.py
@@ -11,14 +11,23 @@ import numarray
 from setup import *
 
 import kolmogorovutils
+import pybnlib
 
 def GenTestK(n):
     return kolmogorovutils.GridToNumarray(kolmogorovutils.GenerateKolmogorov3D(n,n,n,
                                                                                99))
 
+def GenTestK_Balanced(n):
+    opts=pybnlib.KInitialEFB + pybnlib.KBalancedIters+ pybnlib.KEdgeBalanced
+    c=kolmogorovutils.GenerateKolmogorov3D(n,n,n,
+                                           99,
+                                           opts)
+    return kolmogorovutils.GridToNumarray(c)
+
 class TestK3Changes(unittest.TestCase):
 
     testdataname= "data/k3changes_9.p"
+    testbalancedname = "data/k3changes_9_bal.p"
 
     def testCubeChange(self):
 
@@ -28,6 +37,23 @@ class TestK3Changes(unittest.TestCase):
         t= numarray.all(a==x)
 
         self.assert_ ( t)
+
+    def testBalancedChange(self):
+
+        a= pickle.load( open(self.testbalancedname))
+        x=GenTestK_Balanced(9)
+
+        t= numarray.all(a==x)
+        self.assert_ ( t)
+
+    def testBalancedDefault(self):
+
+        a= GenTestK(9)
+        b= GenTestK_Balanced(9)
+
+        t= numarray.all(a==b)
+        self.assert_ ( not t)
+        
         
 
 
