4a8b985fc17d7fb62a04f904fe3829a19af9d3a9
Bojan Nikolic <bojan@bnikolic.co.uk>
2008-01-11 18:51:13 +0000

    Add file outlines for fitting routines

---
+++ astromap/src/fitting_basic.cxx
@@ -0,0 +1,17 @@
+/**
+  \file fitting_basic.cxx
+  Bojan Nikolic <bojan@bnikolic.co.uk>, <bn204@mrao.cam.ac.uk>
+  Initial version January 2008.
+*/
+
+#include "fitting_basic.hxx"
+
+#if HAVE_MINIMMODEL_HXX
+
+namespace AstroMap {
+  
+  
+
+}
+
+#endif //HAVE_MINIMMODEL_HXX
---
+++ astromap/src/fitting_basic.hxx
@@ -0,0 +1,24 @@
+/**
+  \file fitting_basic.hxx
+  Bojan Nikolic <bojan@bnikolic.co.uk>, <bn204@mrao.cam.ac.uk>
+  Initial version January 2008.
+  
+  Basic fitting routines on maps.
+
+*/
+#ifndef _ASTROMAP_FITTING_BASIC_HXX__
+#define _ASTROMAP_FITTING_BASIC_HXX__
+
+#include "../config.h"
+
+#if HAVE_MINIMMODEL_HXX
+
+namespace AstroMap {
+  
+  
+
+}
+
+#endif // HAVE_MINIMMODEL_HXX
+
+#endif
