7fdd037c1ff65532d4d6e4264a7f69956aba243e
Bojan Nikolic <bojan@bnikolic.co.uk>
2008-05-23 16:03:02 +0100

    * bnmin : add the bnmain files

---
+++ bnmin1/src/bnmin_main.cxx
@@ -0,0 +1,20 @@
+/**
+   \file bnmin_main.cxx
+   Bojan Nikolic <bojan@bnikolic.co.uk>
+
+*/
+
+#include "bnmin_main.hxx"
+#include "../config.h"
+
+namespace Minim {
+
+  const char * version(void)
+  {
+    return PACKAGE_VERSION;
+  }
+
+
+}
+
+
---
+++ bnmin1/src/bnmin_main.hxx
@@ -0,0 +1,19 @@
+/**
+   \file bnmin_main.hxx
+   Bojan Nikolic <bojan@bnikolic.co.uk>
+
+   The main include file for BNMin1 Library
+
+*/
+
+#ifndef __BNMIN_BNMIN_MAIN_HPP__
+#define __BNMIN_BNMIN_MAIN_HPP__
+namespace Minim {
+
+
+  const char * version(void);
+
+
+}
+#endif
+
