edd8e2386449ba3492c01e2c190eb39422e9198e
Bojan Nikolic <bojan@bnikolic.co.uk>
2008-06-03 12:52:40 +0100

    * bnlib: More work on documentation for Kolmogorov routines

--- bnlib/src/kolmog/kolmogorov_3d.hxx
+++ bnlib/src/kolmog/kolmogorov_3d.hxx
@@ -13,19 +13,32 @@
 
 #include <stdlib.h>
 
+/// General utilities namespace
 namespace BNLib {
 
   // Forwards
   class RDist;
 
   /**
-     Allow user to pass options to the 3d generation routine.
+     Options for the 3d turbulence generation routies. Has the
+     structure of a bit field.
    */
-  enum Kolmgorov3DOptions { KInitialEFB =0 ,
-			    KInitialFBB =1,
-                            KWeightedInterp = 2,
-                            KBalancedIters  = 4,
-			    KEdgeBalanced   = 8};
+  enum Kolmgorov3DOptions { 
+    /// Generate the inital corners using the exact algorithm
+    /// considering edge, face and body diagonals
+    KInitialEFB =0 ,
+    /// Generate the initial corners using approximate algorithm (not
+    /// recomended)
+    KInitialFBB =1,
+    /// If this bit is set, weigh interpolation by inverse distance to
+    /// parent point
+    KWeightedInterp = 2,
+    /// If this bit is set, only interpolate paraents which have a
+    /// valid symetric parent. This applies to face interpolation
+    KBalancedIters  = 4,
+    /// As KBalancedIters, but for edge interpolation.
+    KEdgeBalanced   = 8
+  };
 
   /**
      Generate 3d komogorov turbulence on Ncubed grid
@@ -35,13 +48,21 @@ namespace BNLib {
 		       RDist &rfn);
 
   /**
-     Generated 3d Kolmogorov turbuluence on a Nx by Ny by Nz grid.
+     Generated 3d Kolmogorov turbuluence on a Nx by Ny by Nz
+     grid. This is the main entry point to the three dimensional
+     Kolmogorov field generation.
      
      \param cube is the pre-allocated output array of size Nx times Ny
      times Nz.
 
      \param Nx,Ny,Nz lenghts in the Nx,Ny,Nz directions. Direction Nx
      must not be smaller than then two other directions.
+     
+     \param rfn the gaussian random number generator to be used by the
+     routie
+
+     \param opt Options for adjusting the generation algorithm. See
+     BNLib::Kolmgorov3DOptions .
 
      \return The maximum order used + 1
    */
