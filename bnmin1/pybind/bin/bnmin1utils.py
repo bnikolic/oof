5b929a99d214789582727ddbbbadfc39e6daafb6
Bojan Nikolic <bojan@bnikolic.co.uk>
2008-06-09 16:53:14 +0100

    * bnmin1 : add some utilities

--- bnmin1/pybind/bin/bnmin1utils.py
+++ bnmin1/pybind/bin/bnmin1utils.py
@@ -4,11 +4,32 @@
 
 import numpy
 
+import pybnmin1
+
 def ChainToArray(c):
     """Extract points from a chain and return as numpy array"""
     res=numpy.zeros( (len(c), len(c[0].p) ))
     for i,x in enumerate(c):
         res[i]=x.p
     return res
+
+def ChainToLkl(c):
+    """Extract the likelihoods from a chain"""
+    res=numpy.zeros( len(c))
+    for i,x in enumerate(c):
+        res[i]=x.ll
+    return res    
+
+def ChainMostLkl(c):
+    """Return the parameters of the most likely point of the chain"""
+    l=ChainToLkl(c)
+    return list(c[l.argmax()].p)
+
+
+def SetIC(m,ic):
+    """ Set the parameters of a model from a list"""
+    mod = pybnmin1.ModelDesc(m.downcast())
+    for i,x in enumerate(ic):
+        mod.getbynumb(i).setp(x)
     
     
