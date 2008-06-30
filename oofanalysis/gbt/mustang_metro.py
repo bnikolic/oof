aabec2cc60a8f759b4b1d1be7dd602cb0f6789ad
Bojan Nikolic <bojan@bnikolic.co.uk>
2008-06-30 14:57:03 +0100

    Update MUSTANG metropolis analysis to write out the chain + documentation

--- oofanalysis/gbt/mustang_metro.py
+++ oofanalysis/gbt/mustang_metro.py
@@ -14,10 +14,28 @@ def MetroMustang(fnamein,
                  amp_sigma=0.00001,
                  z_sigma=0.05,
                  nsample=100,
-                 ic=None):
+                 ic=None,
+                 fnameout_chain=None):
     """
     Sample posterior distribution given MUSTANG data using the
     Metropolis MCMC algorithm
+
+    :param amp_sigma: Standard deviation of the proposal distribution
+    for aperture function amplitude
+    
+    :param z_sigma: Standard deviation of the proposal distribution
+    for the coefficients of Zernikes
+
+    :param nsample: The number of proposals to make
+
+    :param ic: The initial condition to start the chain from. So far
+    support a FITS file containing the fit as output by FSave from
+    bnmin1io
+
+    :param fnameout_chain: Name of FITS file to which the calculated
+    chain should be written to.
+    
+    :returns: The calculated chain 
     """
     
     oc=oofreduce.MkObsCompare( fnamein, 
@@ -29,10 +47,16 @@ def MetroMustang(fnamein,
                                   sigmas,
                                   33)
     if ic is not None:
-        bnmin1io.FLoad(ic)
+        bnmin1io.FLoad(metro,
+                       ic)
     else:
         metro.getbyname("amp").setp(0.0001)
-    return metro.sample(nsample)    
+
+    chain=metro.sample(nsample)    
+    if fnameout_chain:
+        bnmin1io.fSaveChain(metro, chain, 
+                            fnameout_chain)
+    return chain
     
 
 def TestMetro():
@@ -40,5 +64,13 @@ def TestMetro():
     return MetroMustang("td/t18-raw-5-3-db.fits", 
                         amp_sigma=0.00001,
                         z_sigma=0.02,
-                        nsample=10000)
+                        nsample=10)
+
+def TestMetroIC():
+    return MetroMustang("td/t18-raw-5-3-db.fits", 
+                        amp_sigma=0.00001,
+                        z_sigma=0.01,
+                        nsample=10000,
+                        ic="oofout/t18-raw-5-3-db-000/z5/fitpars.fits",
+                        fnameout_chain="temp/metro_ic.fits")
 
