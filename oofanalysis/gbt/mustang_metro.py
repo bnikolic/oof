ab14b60dbb5692d9aab5a2ed4df677bb7665da48
Bojan Nikolic <bojan@bnikolic.co.uk>
2008-06-30 13:16:56 +0100

    Slightly generalise the metropolis + mustang analysis

--- oofanalysis/gbt/mustang_metro.py
+++ oofanalysis/gbt/mustang_metro.py
@@ -6,19 +6,39 @@ import setup
 
 import pybnmin1
 import bnmin1utils
+import bnmin1io
 
 import oofreduce
 
+def MetroMustang(fnamein,
+                 amp_sigma=0.00001,
+                 z_sigma=0.05,
+                 nsample=100,
+                 ic=None):
+    """
+    Sample posterior distribution given MUSTANG data using the
+    Metropolis MCMC algorithm
+    """
+    
+    oc=oofreduce.MkObsCompare( fnamein, 
+                               nzern=5)
+
+    sigmas=pybnmin1.DoubleVector([ amp_sigma]+ [z_sigma]*20 )
+
+    metro=pybnmin1.MetropolisMCMC(oc.downcast(),
+                                  sigmas,
+                                  33)
+    if ic is not None:
+        bnmin1io.FLoad(ic)
+    else:
+        metro.getbyname("amp").setp(0.0001)
+    return metro.sample(nsample)    
+    
 
 def TestMetro():
 
-    oc=oofreduce.MkObsCompare( "td/t18-raw-5-3-db.fits", 
-                               nzern=5)
+    return MetroMustang("td/t18-raw-5-3-db.fits", 
+                        amp_sigma=0.00001,
+                        z_sigma=0.02,
+                        nsample=10000)
 
-    sigmas=oofreduce.pybnmin1.DoubleVector([ 0.00001]+ [0.05]*20 )
-
-    metro=oofreduce.pybnmin1.MetropolisMCMC(oc.downcast(),
-                                            sigmas,
-                                            33)
-    metro.getbyname("amp").setp(0.0001)
-    return metro.sample(100)    
