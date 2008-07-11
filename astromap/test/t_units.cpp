36aebdf65c3bee3e3cae89123e2435aa625c28b4
Bojan Nikolic <bojan@bnikolic.co.uk>
2008-07-11 10:20:10 +0100

    Start on testing framework

---
+++ astromap/test/t_units.cpp
@@ -0,0 +1,19 @@
+/**
+   \file t_units.cpp
+   
+   Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>, <bojan@bnikolic.co.uk>
+   
+   Unit tests for AstroMap
+*/
+
+#define BOOST_AUTO_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+
+BOOST_AUTO_TEST_CASE(Test1)
+{
+
+}
