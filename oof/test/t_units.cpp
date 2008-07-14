2d2f821fffad783f3f502882f57d875100de3c3b
Bojan Nikolic <bojan@bnikolic.co.uk>
2008-07-14 10:27:05 +0100

    Add c++ unit-test framework to OOF

---
+++ oof/test/t_units.cpp
@@ -0,0 +1,20 @@
+/**
+   \file t_units.cpp
+   
+   Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>, <bojan@bnikolic.co.uk>
+   
+   Unit tests for OOF
+*/
+
+#define BOOST_AUTO_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+
+#include <boost/scoped_ptr.hpp>
+
+BOOST_AUTO_TEST_CASE(t_empty)
+{
+
+}
