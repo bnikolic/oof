53a5b7166aeff9fba2db39217c919824bb1fd931
Bojan Nikolic <bojan@bnikolic.co.uk>
2008-08-22 13:20:26 +0100

    * oof : Update build manual, script

--- oof/doc/manual/oofbuild.sh
+++ oof/doc/manual/oofbuild.sh
@@ -1,19 +1,20 @@
 # Bojan Nikolic <bojan@bnikolic.co.uk>
-# Helper script to build oof
+#
+# Bash script that illustrates building the OOF application
 
-export CPPFLAGS="${CPPFLAGS} -I${OOFPREFIX}/include"
-export CFLAGS="${CFLAGS} -I${OOFPREFIX}/include" 
-export SWIGFLAGS="-I${OOFPREFIX}/include" 
-export LDFLAGS="${LDFLAGS} -L${OOFPREFIX}/lib"
+# Note: we are now using the pkg-config utility, simplifying
+# installation considerably
 
-COMPS="bnlib-1.1 bnfits-1.1 astromap-1.1 bnmin1-1.1.1 oof-1.1"
+export PKG_CONFIG_PATH=${OOFPREFIX}/lib/pkgconfig/
+
+COMPS="bnlib-1.2.1 bnfits-1.2 astromap-1.2 bnmin1-1.3 oof-1.2"
 
 for x in ${COMPS}
 do
   if  [ -e ${x}.tar.bz2 ] ;  then 
   tar xvjf ${x}.tar.bz2  &&
   (cd ${x} &&
-      ./configure --prefix=${OOFPREFIX} && 
+      ./configure --prefix=${OOFPREFIX} ${BOOSTFLAG} && 
       make install
       )
   else
