2da5a7611ecec5b02fb8297e8affd49f680228bf
Bojan Nikolic <bojan@bnikolic.co.uk>
2008-11-04 10:05:05 +0000

    More work on Gaussian fitting illustrations

--- astromaptest/gaussfitting.py
+++ astromaptest/gaussfitting.py
@@ -2,6 +2,7 @@
 Bojan Nikolic <bojan@bnikolic.co.uk>
 """
 import numpy
+import pickle
 
 import setup
 
@@ -11,6 +12,7 @@ import bnmin1utils
 import pybnmin1
 
 import pyxplot
+from pyhlp import obsdst, twod
 
 
 def MkIllMap(npix,
@@ -20,7 +22,7 @@ def MkIllMap(npix,
     fm=pyplot.GaussMapModel(m1)
     fm.worldcs=False
     bnmin1utils.SetIC(fm, [1,npix/2,npix/2,
-                           70,0.5,0.3])
+                           width,0.5,0.3])
     fm.eval(m1)
     mnoise=pyplot.Map(npix, npix)
     pyplot.NormDist(mnoise, 1.0 * noise)
@@ -51,9 +53,16 @@ def plotProjections(m):
                  
     return xproj, yproj
 
-if 1:
+if 0:
     m=MkIllMap(256)
     x,y=plotProjections(m)
+
+def plotmapmc():
+    for i in range(10):
+        implot.plotmap(MkIllMap(64,0.5),
+                       "o/gauss-5-%i.eps/CPS" % i,
+                       colmap="heat")
+
 
 def maxpix(m):
     d=numpy.array(m.vect())
@@ -75,26 +84,61 @@ def plotmode(n=1000):
                   
     return xx, xy
     
-def MkMapFit(m):
+def MkMapFit(m,
+             width=70):
     fm=pyplot.GaussMapModel(m)
     bnmin1utils.SetIC(fm, [1,m.nx/2,m.ny/2,
-                           70,0.5,0.3])
+                           width,0.5,0.3])
     lmm=pybnmin1.LMMin(fm)
     lmm.solve()
-    return [ lmm.getbyname(p).getp() for p in ["amp"]]
+    return [ lmm.getbyname(p).getp() for p in ["amp", "sigma"] ]
 
-def PlotAmpBias(npix=64,n=100,noise=0.5):
+
+def GenAmpBiasData(npix=64,n=100,noise=0.5):
     r=[]
+    r2=[]
     for i in range(n):
         r.append(MkMapFit(MkIllMap(npix,noise=noise))[0])
-    pyxplot.histogram([[r, ""]],
+        r2.append(MkMapFit(MkIllMap(npix,noise=noise/(2.0**0.5),width=70.0/2),
+                           width=70.0/2)[0])
+    pickle.dump([r,r2],open("o/ampbiasdata.p","w"))
+
+def GenCorrData(npix=64,n=100,noise=0.5):
+    r=[]
+    for i in range(n):
+        r.append(MkMapFit(MkIllMap(npix,noise=noise)))
+    pickle.dump(r,open("o/corrdata.p","w"))
+
+
+def PlotAmpBias():
+    r,r2=pickle.load(open("o/ampbiasdata.p"))
+    noise=0.5
+    pyxplot.histogram([[r, ""],
+                       [r2,""]],
                       "o/amphist-%i.eps" % (noise*10,),
                       width=10,
                       xlabel="Amplitude",
                       nbins=50)    
 
+    obsdst.cuml([[r, ""],
+                 [r2,""]],
+                "o/ampcuml-%i.eps" % (noise*10,),
+                xlabel="Amplitude",
+                fmin=0.4,
+                fmax=0.6)
+
+    obsdst.cuml([[r, ""],
+                 [r2,""]],
+                "o/ampcuml-%i-whole.eps" % (noise*10,),
+                xlabel="Amplitude")
 
 
+def plotCorr():
+    d=numpy.array(pickle.load(open(("o/corrdata.p"))))
+    twod.hist2d(d[:,0],d[:,1],
+                "o/ampwidthcorr.eps",
+                xlabel="Amplitude",
+                ylabel="Width")
 
 
 
