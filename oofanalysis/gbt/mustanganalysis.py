0a07b53dde7e23a3f4adeb9cd231c1e23b7c0289
Bojan Nikolic <bojan@bnikolic.co.uk>
2008-11-24 12:49:50 +0000

    More on mustang analysis

--- oofanalysis/gbt/mustanganalysis.py
+++ oofanalysis/gbt/mustanganalysis.py
@@ -6,19 +6,19 @@ More analalysis of Mustang data
 
 import os
 
+import setup
+
 import oofreduce
 import oofplot
 
 import mustang
 
-
-
-
 def PrepSinglePixelData(fnamein,
                         col,row,
+                        post="",
                         do90=False):
 
-    ffout="td/px-%i-%i.fits"% (col,row)
+    ffout="td/px-%i-%i-%s.fits"% (col, row, post)
     mustang.SinglePixelFile(fnamein,
                             ffout,
                             col, row)    
@@ -26,6 +26,30 @@ def PrepSinglePixelData(fnamein,
     mustang.CorrectDZ(ffout)    
     if do90:
         mustang.SetTelsc(ffout,"90GBT")
+
+def MkTPar18SinglePx(col,row,
+                     qd=False,
+                     reddate="21nov08",
+                     datain="/home/bnikolic/data/gbt-oof/mustang4/"):
+    if qd:
+        post="qd"
+    else:
+        post="noqd"
+    fnamein=os.path.join(datain,
+                         "tpar18s56s58-%s-p12-%s0854+2006.fits" %(reddate,
+                                                                  post))
+    PrepSinglePixelData(fnamein,
+                        col,
+                        row,
+                        post=post,
+                        do90=True)
+
+    
+                        
+    
+    
+    
+    
 
 
 
