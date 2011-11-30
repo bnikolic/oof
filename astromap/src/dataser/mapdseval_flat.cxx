856f9d7a646ae7f9e3ae17083af4953d45a35eb0
Bojan Nikolic <bojan@bnikolic.co.uk>
2011-11-30 18:40:01 +0000

    Initial sketch of the flat data mapds eval

---
+++ astromap/src/dataser/mapdseval_flat.cxx
@@ -0,0 +1,55 @@
+/**
+   \file mapdseval_flat.cxx
+
+   Bojan Nikolic <bn204@mrao.cam.ac.uk> , <bojan@bnikolic.co.uk>
+
+   Initial version November 2011
+
+*/
+
+#include <memory>
+
+#include "mapdseval_flat.hxx"
+#include "dataseries.hxx"
+
+namespace AstroMap {
+
+  MapDSEvalFlat::MapDSEvalFlat(DataSeries const & ds, 
+                               Map const & msample,
+                               double fwhm_px, 
+                               double extent_px):
+    c_stride((extent_px+1)*(extent_px+1)),
+    ndp(ds.size()),
+    nt(ndp * c_stride),
+    iv(nt), cv(nt, 0.0)
+  {
+    for (size_t i =0 ; i < ds.size() ; ++i )
+    {
+      std::auto_ptr<MapPixLC> t (MkGaussCoffs( ds[i].dX, ds[i].dY, 
+                                               msample,
+                                               fwhm_px, extent_px));
+      
+      for (size_t j=0 ; j < t->index.size(); ++j)
+      {
+        iv[c_stride*i+j]=t->index[j];
+        cv[c_stride*i+j]=t->coeff[j];
+      }
+    }
+  }
+
+  void MapDSEvalFlat::Calc( Map const &m, 
+                            std::valarray<double> &res)
+  {
+    for(size_t i=0; i<ndp; ++i)
+    {
+      res[i]=0;
+      double t=0;
+      for(size_t j=0; j<c_stride; ++j)
+      {
+        t+=cv[i*c_stride+j] * m[ iv[i*c_stride+j] ];
+      }
+      res[i]=t;
+    }
+  }
+}
+
---
+++ astromap/src/dataser/mapdseval_flat.hxx
@@ -0,0 +1,44 @@
+/**
+   \file mapdseval_flat.hxx
+
+   Bojan Nikolic <bn204@mrao.cam.ac.uk> , <bojan@bnikolic.co.uk>
+
+   Initial version November 2011
+
+   Interpolation of maps, but with the coefficients etc in flat data
+   structures (minimal heap allocation) to optimise memore locality
+*/
+#ifndef _ASTROMAP_MAPDSEVAL_FLAT_HXX__
+#define _ASTROMAP_MAPDSEVAL_FLAT_HXX__
+
+#include <vector>
+
+#include "mapdseval.hxx"
+
+namespace AstroMap {
+
+  class MapDSEvalFlat {
+
+    /** Stride in coefficients to get to next return value
+     */
+    const size_t c_stride;
+    const size_t ndp;
+    const size_t nt;
+
+    std::vector<double> iv, cv;
+
+  public:
+    
+    MapDSEvalFlat(DataSeries const & ds , 
+                  Map const & msample,
+                  double fwhm_px, 
+                  double extent_px);
+
+    void Calc( Map const &m, 
+	       std::valarray<double> & res);
+    
+  };
+
+}
+
+#endif
