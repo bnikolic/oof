adc073ffdac4c278fc948946c38405bd5861ece6
Bojan Nikolic <bojan@bnikolic.co.uk>
2012-01-24 12:20:09 +0000

    More work on seeing simulation

--- astromaptest/seeingturbulence.py
+++ astromaptest/seeingturbulence.py
@@ -4,6 +4,7 @@
 """
 Make some simulations of astronomical seeing using turbulenent phase screens
 """
+import numpy 
 
 from localsetup import *
 
@@ -20,7 +21,10 @@ thickscreen_name="/home/bnikolic/d/n/almawvr/py/temp/atmo_em_screen_33.fits"
 m1=pyplot.FitsMapLoad(thickscreen_name,1)
 m2=pyplot.MapSubset(m1, 0, 128, 0, 128)
 
-def mkAmp(npix):
+def mkAmp(npix,
+          radius=0.5,
+          x0=0,
+          y0=0):
     """
     """
     m=pyplot.Map(npix, npix)
@@ -28,7 +32,9 @@ def mkAmp(npix):
                     1,
                     1)
     pfn=pybnlib.TopHatDD()
-    pfn.radius=0.5
+    pfn.radius=radius
+    pfn.x0=x0
+    pfn.y0=y0
     pyplot.WorldSet(m,
                     pfn)
     return m
@@ -51,9 +57,53 @@ def PSFSeq():
                        colmap="heat",
                        fout="plots/psf-seq-%03i.png/PNG" % o)
         acc.add(mm)
+        m2=pyplot.MapSubset(m1, o, o+128, 0, 128)    
+        m2.mult(0.15)
+        implot.plotmap(m2, transf=2, 
+                       colmap="heat",
+                       fout="plots/screen-seq-%03i.png/PNG" % o)
+
     implot.plotmap(acc, transf=2, 
                    colmap="heat",
                    fout="plots/psf-accum.png/PNG")
+
+def SHil(o=0):
+    """
+    Illustrate Shack-Hartman principle
+    """
+    m2=pyplot.MapSubset(m1, o, o+128, 0, 128)    
+    m2.mult(0.15)
+    mres=pyplot.Map(128,128)
+    implot.plotmap(m2, colmap="heat", fout="plots/sh-screen-phase.png/PNG")
+    implot.plotmap(ma, colmap="heat", fout="plots/sh-screen-amplitude.png/PNG")
+    ff.FFTAmpPh_Power(ma, m2, mres)
+    implot.plotmap(mres, transf=2,colmap="heat", fout="plots/sh-psfwhole.png/PNG")
+
+    ma2=mkAmp(128, radius=0.2)
+    implot.plotmap(ma2, colmap="heat", fout="plots/sh-screen-amplitude-l1.png/PNG")
+    ff.FFTAmpPh_Power(ma2, m2, mres)
+    implot.plotmap(mres, transf=2,colmap="heat", fout="plots/sh-psfl1.png/PNG")
+
+    ma2=mkAmp(128, radius=0.1)
+    implot.plotmap(ma2, colmap="heat", fout="plots/sh-screen-amplitude-l2.png/PNG")
+    ff.FFTAmpPh_Power(ma2, m2, mres)
+    implot.plotmap(mres, transf=2,colmap="heat", fout="plots/sh-psfl2.png/PNG")
+
+    for i in range(7):
+        for j in range(7):
+            ma2=mkAmp(128, radius=0.1, 
+                      x0=-0.4*0.1*i,
+                      y0=-0.4*0.1*j)
+            implot.plotmap(ma2, colmap="heat", fout="plots/sh-screen-amplitude-ll%i-%i.png/PNG" % (i,j))
+            ff.FFTAmpPh_Power(ma2, m2, mres)
+            implot.plotmap(mres, transf=0,colmap="heat", fout="plots/sh-psfll-%i-%i.png/PNG" % (i,j), 
+                           contours=implot.MkSynthesisContours(mres, step=0.5, nlevels=4),
+                           bbox=[40,88,40,88])
+            
+    
+    
+    
+    
 
 
 
