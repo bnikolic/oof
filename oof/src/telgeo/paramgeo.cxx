5cfea7eba02378cd55ba9633608416d33b1a8363
Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
2018-01-15 06:30:55 +0000

    Begin on parametrised geometry
    Tidy up the gitignor

---
+++ oof/src/telgeo/paramgeo.cxx
@@ -0,0 +1,10 @@
+/**
+   2018 Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
+*/
+
+
+#include "paramgeo.hxx"
+
+namespace OOF {
+
+}
---
+++ oof/src/telgeo/paramgeo.hxx
@@ -0,0 +1,25 @@
+/**
+   \file paramgeo.hxx
+   2018 Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
+*/
+#ifndef _OOF_TELGEO_PARAMGEO_HXX__
+#define _OOF_TELGEO_PARAMGEO_HXX__
+
+#include "telgeo.hxx"
+
+namespace OOF {
+
+  /**
+     \brief Geometry specified as a Zernike parametrisation
+     
+     Use for shaped antennas 
+  */
+  class  ParamGeo :  
+    public  TelGeometry 
+  {
+    
+  };
+
+}
+
+#endif
