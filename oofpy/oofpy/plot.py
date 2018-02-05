c0ee1e39859c4d0c81fa5d9c6d2f8fbc94fde59d
Bojan Nikolic <bojan@bnikolic.co.uk>
2018-02-05 14:11:27 +0000

    Add plot for aperture plane

--- oofpy/oofpy/plot.py
+++ oofpy/oofpy/plot.py
@@ -2,6 +2,19 @@
 
 import matplotlib.pyplot as p
 
+def aplane(a, ph, fnameout="aplane.png"):
+    fig=p.figure(figsize=(7,3))
+    ax=fig.add_subplot(1, 2, 1)
+    m=ax.imshow(a)
+    p.colorbar(m)
+    p.title("Aperture plane amplitude")
+    ax=fig.add_subplot(1, 2, 2)
+    m=ax.imshow(ph)
+    p.colorbar(m)
+    p.title("Aperture plane phase")    
+    p.savefig(fnameout)
+    p.close()
+
 def beams(a, fnameout="beams.png"):
     fig=p.figure()
     N=a.shape[0]
