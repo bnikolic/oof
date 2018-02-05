4e5455f4ecbf67e4915ea3a9012406d385548945
Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
2018-02-05 13:34:26 +0000

    Factor out the prediction function, beam noise

--- oofpy/examples/accuracy.py
+++ oofpy/examples/accuracy.py
@@ -7,6 +7,31 @@ from oofpy import zernike, amp, oof, telgeo, plot
 
 from matplotlib import pylab
 
+def mkPredFn(dz,
+             nzern,
+             wl,
+             NN,
+             oversamp):
+    "Wrap the predictor fn"
+    R=3.0
+    g=numpy.moveaxis(numpy.mgrid[-2*oversamp:2*oversamp:NN*1j,
+                                 -2*oversamp:2*oversamp:NN*1j], 0, -1)
+    dztemp=telgeo.primeF(14.4, g*R)
+    dzl=numpy.array([dztemp*0,
+                     dztemp*-1.0*dz/wl * 2 *numpy.pi,
+                     dztemp*dz/wl * 2 *numpy.pi])
+
+    nzpoly=zernike.N(nzern)
+    initv=[0]*nzpoly + [0,0,1,0.3, 0, 0]
+
+    f,pl=oof.mkPredFn(nzern,
+                      g,
+                      dzl,
+                      numpy.array(initv),
+                      omitp=["z0", "rho", "diff"])
+    p0=numpy.array([0]*(nzpoly-1) + [0,0, 1,0.3])
+    return f, pl, p0
+
 def retErr(dz,
            nzern=4,
            nsim=50,
@@ -27,23 +52,12 @@ def retErr(dz,
     :param dz: Defocus
 
     """
-    R=3.0
-    g=numpy.moveaxis(numpy.mgrid[-2*oversamp:2*oversamp:NN*1j,
-                                 -2*oversamp:2*oversamp:NN*1j], 0, -1)
-    dztemp=telgeo.primeF(14.4, g*R)
-    dzl=numpy.array([dztemp*0,
-                     dztemp*-1.0*dz/wl * 2 *numpy.pi,
-                     dztemp*dz/wl * 2 *numpy.pi])
+    f,pl,p0=mkPredFn(dz=dz,
+                     nzern=nzern,
+                     wl=wl,
+                     NN=NN,
+                     oversamp=oversamp)
 
-    nzpoly=zernike.N(nzern)
-    initv=[0]*nzpoly + [0,0,1,0.3, 0, 0]
-
-    f,pl=oof.mkPredFn(nzern,
-                      g,
-                      dzl,
-                      numpy.array(initv),
-                      omitp=["z0", "rho", "diff"])
-    p0=numpy.array([0]*(nzpoly-1) + [0,0, 1,0.3])
     # Use bclean to compute size of beam for ideal telescope
     bclean=f(p0)
     # Factor noise /beam -> /pixel  bclean[0].sum()/(bclean[0]**2).sum()**0.5
@@ -64,6 +78,18 @@ def retErr(dz,
                                      full_output=True)
             res.append( x[0])
     return numpy.vstack(res)
+
+def noiseF(wl=1.1e-3,
+           NN=256,
+           oversamp=2.0):
+    """Calculate the factor by which  per-pixel noise  is greater than the per-beam noise"""
+    f,pl,p0=mkPredFn(dz=0,
+                     nzern=1,
+                     wl=wl,
+                     NN=NN,
+                     oversamp=oversamp)
+    bclean=f(p0)
+    return bclean[0].sum()/((bclean[0]**2).sum()**0.5)
 
 if 0:
     xx=retErr(50e-3, nsim=500)
