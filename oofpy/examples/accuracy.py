9807e961ad611ddad2e0db77248205a871958c09
Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
2018-02-05 10:01:02 +0000

    Add number of zernikes as parameter

--- oofpy/examples/accuracy.py
+++ oofpy/examples/accuracy.py
@@ -1,18 +1,31 @@
 import numpy
-import scipy.optimize
+import scipy.optimize, scipy.stats
+import pymp
 
 import oofpy
 from oofpy import zernike, amp, oof, telgeo, plot
 
+from matplotlib import pylab
+
 def retErr(dz,
+           nzern=4,
            nsim=50,
            wl=1.1e-3,
            NN=256,
            oversamp=2.0):
     """Compute retrieval error
 
-    :param dz: Defocus
+    :param nzern: Maximum order of zernikes to include
     
+    :param oversamp: Oversampling with respect to the Nyqyist sampling
+    in the beam domain. *Note change of convention wrt OOF C++ code*
+    
+    :param nsim: Number of simulations to make
+
+    :param NN: Number of pixels along the side of the beam maps
+    
+    :param dz: Defocus
+
     """
     R=3.0
     g=numpy.moveaxis(numpy.mgrid[-2*oversamp:2*oversamp:NN*1j,
@@ -22,39 +35,46 @@ def retErr(dz,
                      dztemp*-1.0*dz/wl * 2 *numpy.pi,
                      dztemp*dz/wl * 2 *numpy.pi])
 
-    initv=[0]*6 + [0,0,1,0.3, 0, 0]
+    nzpoly=zernike.N(nzern)
+    initv=[0]*nzpoly + [0,0,1,0.3, 0, 0]
 
-    f,pl=oof.mkPredFn(2, g, dzl, numpy.array(initv),
-                      omitp=["z0", "x0", "y0"])
-    p0=numpy.array([0]*5 + [1,0.3, 0, 0])
+    f,pl=oof.mkPredFn(nzern,
+                      g,
+                      dzl,
+                      numpy.array(initv),
+                      omitp=["z0", "rho", "diff"])
+    p0=numpy.array([0]*(nzpoly-1) + [0,0, 1,0.3])
     # Use bclean to compute size of beam for ideal telescope
     bclean=f(p0)
     # Factor noise /beam -> /pixel  bclean[0].sum()/(bclean[0]**2).sum()**0.5
 
     borig=f(p0)
 
-    res=[]
-    for i in range(nsim):
-        bnoise=borig+numpy.random.normal(0,
-                                         borig.max()*0.05,
-                                         borig.shape)
-        def fitfn(pars):
-            return (bnoise-f(pars)).flatten()
+    res=pymp.shared.list()
+    with pymp.Parallel(4) as p:
+        for i in p.range(nsim):
+            bnoise=borig+numpy.random.normal(0,
+                                             borig.max()*0.05,
+                                             borig.shape)
+            def fitfn(pars):
+                return (bnoise-f(pars)).flatten()
 
-        x=scipy.optimize.leastsq(fitfn,
-                                 p0,
-                                 full_output=True)
-        res.append( x[0])
+            x=scipy.optimize.leastsq(fitfn,
+                                     p0,
+                                     full_output=True)
+            res.append( x[0])
     return numpy.vstack(res)
 
 if 0:
     xx=retErr(50e-3, nsim=500)
-    numpy.savez("simaccbasic.npz", xx)
+    numpy.savez("simaccbasic3.npz", xx)
 
 def plotParDist(xx):
-    for j in range(xx.shape[0]):
+    for j in range(xx.shape[1]):
         pylab.clf()
         pylab.hist(xx[:,j])
         pylab.savefig(("par-%i-dist.png" % j))
     
+#        scipy.mstats.normaltest(a, axis=0)[source]¶
+        
         
