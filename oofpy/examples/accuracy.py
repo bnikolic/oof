d2a9a1918daff331fd387dcb701e443aad8606d7
Bojan Nikolic <bojan@bnikolic.co.uk>
2018-02-05 14:44:43 +0000

    Calculation of weighted RMS from simulations

--- oofpy/examples/accuracy.py
+++ oofpy/examples/accuracy.py
@@ -1,3 +1,5 @@
+import imp # for reload
+
 import numpy
 import scipy.optimize, scipy.stats
 import pymp
@@ -6,6 +8,9 @@ import oofpy
 from oofpy import zernike, amp, oof, telgeo, plot
 
 from matplotlib import pylab
+
+NTHREAD=20
+SIGMAIL=0.72
 
 def mkPredFn(dz,
              nzern,
@@ -22,14 +27,14 @@ def mkPredFn(dz,
                      dztemp*dz/wl * 2 *numpy.pi])
 
     nzpoly=zernike.N(nzern)
-    initv=[0]*nzpoly + [0,0,1,0.3, 0, 0]
+    initv=[0]*nzpoly + [0,0,1, SIGMAIL, 0, 0]
 
     f,pl=oof.mkPredFn(nzern,
                       g,
                       dzl,
                       numpy.array(initv),
                       omitp=["z0", "rho", "diff"])
-    p0=numpy.array([0]*(nzpoly-1) + [0,0, 1,0.3])
+    p0=numpy.array([0]*(nzpoly-1) + [0,0, 1, SIGMAIL])
     return f, pl, p0
 
 def retErr(dz,
@@ -37,7 +42,9 @@ def retErr(dz,
            nsim=50,
            wl=1.1e-3,
            NN=256,
-           oversamp=2.0):
+           oversamp=2.0,
+           noisesn=0.05,
+           noisemult=0.05):
     """Compute retrieval error
 
     :param nzern: Maximum order of zernikes to include
@@ -65,11 +72,18 @@ def retErr(dz,
     borig=f(p0)
 
     res=pymp.shared.list()
-    with pymp.Parallel(4) as p:
+    with pymp.Parallel(NTHREAD) as p:
+        numpy.random.seed()
         for i in p.range(nsim):
-            bnoise=borig+numpy.random.normal(0,
-                                             borig.max()*0.05,
-                                             borig.shape)
+            bmax=borig.max()
+            # Multiplicative noise
+            bnoise=borig*(1+numpy.random.normal(0,
+                                                noisemult,
+                                                borig.shape))
+            # Additive noise
+            bnoise+=numpy.random.normal(0,
+                                        bmax*noisesn,
+                                        borig.shape)
             def fitfn(pars):
                 return (bnoise-f(pars)).flatten()
 
@@ -91,9 +105,41 @@ def noiseF(wl=1.1e-3,
     bclean=f(p0)
     return bclean[0].sum()/((bclean[0]**2).sum()**0.5)
 
+def calcAp(p,
+           nzern=4,
+           NN=256,
+           oversamp=2.0):
+    R=3.0
+    g=numpy.moveaxis(numpy.mgrid[-2*oversamp:2*oversamp:NN*1j,
+                                 -2*oversamp:2*oversamp:NN*1j],
+                     0,
+                     -1)
+    zlc=zernike.mkCFn(nzern, g)
+    nzpoly=zernike.N(nzern)    
+    return (zlc(numpy.array([0]+list(p[0:nzpoly-1]))) * amp.dish (1.0, 0, g) ,
+            amp.gauss(0,0,1, SIGMAIL, 0, 0, g) * amp.dish (1.0, 0, g))
+
+def weightedRMS(a, p):
+    return ( p**2*a/a.sum() ).sum()**0.5
+
+
+def wrms(xx):
+    res=[]
+    for i in range(xx.shape[0]):
+        p, a = calcAp(xx[i])
+        res.append(weightedRMS(a, p))
+    return numpy.array(res)
+
+
+    
+
 if 0:
-    xx=retErr(50e-3, nsim=500)
+    xx=retErr(50e-3, nsim=100, oversamp=1.0, noisesn=0.05*noiseF(oversamp=1.0))
     numpy.savez("simaccbasic3.npz", xx)
+
+if 1:
+    xx=retErr(50e-3, nsim=100, oversamp=2.0, noisesn=0.05*noiseF(oversamp=2.0))
+    numpy.savez("simaccbasic4.npz", xx)
 
 def plotParDist(xx):
     for j in range(xx.shape[1]):
@@ -101,6 +147,7 @@ def plotParDist(xx):
         pylab.hist(xx[:,j])
         pylab.savefig(("par-%i-dist.png" % j))
     
-#        scipy.mstats.normaltest(a, axis=0)[source]¶
+
+#scipy.stats.mstats.normaltest(xx, axis=0)
         
         
