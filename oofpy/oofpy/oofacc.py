5b13225c8f11b88f3c45bec6d8f8269b1e97dee6
Bojan Nikolic <b.nikolic@mrao.cam.ac.uk>
2018-03-26 16:21:35 +0100

    Add the tosky function

--- oofpy/oofpy/oofacc.py
+++ oofpy/oofpy/oofacc.py
@@ -1,6 +1,8 @@
 # Accelerated OOF
 
 import numpy
+import pytorch_fft.fft.autograd as fft
+from torch.autograd import Variable
 
 def cfftshift(x, axes=None,
               invert=False):
@@ -27,4 +29,15 @@ def cfftshift(x, axes=None,
 
 def cifftshift(*args, **kwargs):
     return cfftshift(*args, invert=True, **kwargs)
+
+# E.g. compare with: tosky(x,y).data.cpu().numpy() - oofpy.oof.tosky(x.data.cpu().numpy(),y.data.cpu().numpy())
+def tosky(a, p):
+    Ft=fft.Ifft2d()
+    s=a*torch.cos(p)
+    c=a*torch.sin(p)
+    R=Ft(torch.unsqueeze(cifftshift(s), 0),
+         torch.unsqueeze(cifftshift(c), 0))
+    S=map(cfftshift, R)
+    return S[0]**2 + S[1]**2
+
     
